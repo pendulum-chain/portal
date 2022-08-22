a4d8f86a68d5dbbe16d96045a839e9678fa84e36
Nicolas Danelon <nicolasdanelon@gmail.com>
2022-08-22 09:50:38 -0300

    styles: fix some sizes

--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,5 @@
 import * as React from 'preact';
-import { Routes, Route } from "react-router-dom";
+import { Navigate, Routes, Route } from "react-router-dom";
 import { NotFound } from "./components/NotFound";
 import { Dashboard } from "./pages/dashboard/Dashboard";
 import { Staking } from "./pages/staking/Staking";
@@ -8,10 +8,14 @@ import Layout from "./components/Layout";
 
 export function App() {
   return (
-    <div style={{ maxWidth: 1300, marginLeft: 'auto', marginRight: 'auto' }}>
+    <div style={{ maxWidth: 1800, marginLeft: 'auto', marginRight: 'auto' }}>
       <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Dashboard />} />
+        <Route
+          path="/"
+          element={<Navigate to="/amplitude/dashboard" replace />}
+        />
+        <Route path="/:network/" element={<Layout />}>
+          <Route path="dashboard" element={<Dashboard />} />
           <Route path="staking" element={<Staking />} />
           <Route path="swap" element={<Swap />} />
           <Route path="*" element={<NotFound />} />
