48a7a6886baff5a9b3ea8fccd733f9e2672012c2
Nicolas Danelon <nicolasdanelon@gmail.com>
2022-08-22 09:50:59 -0300

    ts: miscellaneous

--- src/preact.d.ts
+++ src/preact.d.ts
@@ -1,1 +1,13 @@
 import JSX = preact.JSX
+
+type SetStateAction<S> = S | (prevState: S) => S;
+
+type Dispatch<A> = (action: A) => void;
+
+type ReactText = string | number;
+type ReactChild = ReactElement | ReactText;
+
+interface ReactNodeArray extends Array<ReactNode> { }
+type ReactFragment = {} | ReactNodeArray;
+
+type ReactNode = ReactChild | ReactFragment | ReactPortal | boolean | null | undefined;
