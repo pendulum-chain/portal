e72d754604594ccf1b07d22f84020b77e71b0946
Gonza <gonzalo@satoshipay.io>
2023-01-24 14:36:35 -0300

    eslint --fix

--- src/helpers/parseNumbers.ts
+++ src/helpers/parseNumbers.ts
@@ -2,7 +2,7 @@ import BN from "bn.js";
 
 export const UNIT_PRECISION = 1000000000000;
 
-export const toUnit = (value: BigInt) => {
+export const toUnit = (value: bigint) => {
   const bigIntValue = new BN(value.toString());
   const divisor = new BN(UNIT_PRECISION);
   const quotient = bigIntValue.div(divisor);
@@ -10,7 +10,7 @@ export const toUnit = (value: BigInt) => {
   return quotient.toNumber();
 };
 
-export const prettyNumbers = (number: number, lang: string = "en-US") =>
+export const prettyNumbers = (number: number, lang = "en-US") =>
   number.toLocaleString(lang, {
     minimumFractionDigits: 2,
   });
