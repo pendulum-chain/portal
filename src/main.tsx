79a817fff99a52dbda2f1c425390af9c49d3b6f7
Marcel Ebert <mail@marcel-ebert.de>
2023-01-25 18:28:00 +0100

    Change talisman dependencies

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,6 @@ import { GlobalStateProvider } from "./GlobalStateProvider";
 import { App } from "./app";
 import { BrowserRouter } from "react-router-dom";
 import { Theme } from "react-daisyui";
-import "@talisman-connect/components/talisman-connect-components.esm.css";
-import "@talisman-connect/ui/talisman-connect-ui.esm.css";
 import "./index.css";
 import { NodeInfoProvider } from "./NodeInfoProvider";
 
