a77b5c53be82156328ee7450e46e7f51efcace1e
Marcel Ebert <mail@marcel-ebert.de>
2023-02-02 13:56:03 +0100

    Change amplitude theming

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,7 +3,7 @@ module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx}', 'node_modules/daisyui/dist/**/*.js', 'node_modules/react-daisyui/dist/**/*.js'],
   // eslint-disable-next-line no-undef
   plugins: [require('daisyui')],
-  themes: ['light', 'dark', "pendulum", "amplitude"],
+  themes: ["pendulum", "amplitude"],
   daisyui: {
     themes: [
       {
@@ -20,20 +20,23 @@ module.exports = {
           "--btn-text-case": "none",
         },
         amplitude: {
-          background: "#202020",
-          primary: "#00F197",
-          "primary-content": "#fff",
+          primary: "#4EE59A",
+          "primary-content": "#ffffff",
           secondary: "#F4F5F6",
           "secondary-content": "#58667E",
           accent: "#00F197",
-          neutral: "#EFF2F5",
-          green: "hsla(137, 91%, 41%, 1)",
+          neutral: "#191D24",
+          "accent-content": "#ffffff",
+          "neutral-focus": "#111318",
+          "neutral-content": "#A6ADBB",
+          "base-100": "#202020",
+          "base-200": "#1C1C1C",
+          // "base-300": "#20252E",
+          "base-content": "#FFFFFF",
           "--rounded-btn": "9px",
           "--btn-text-case": "none",
         }
       },
-      "dark",
-      "cupcake",
     ],
   }
 };
