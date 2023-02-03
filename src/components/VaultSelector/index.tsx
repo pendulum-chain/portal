e10a924012cf5db8111523e67426b642c707b80e
Marcel Ebert <mail@marcel-ebert.de>
2023-02-03 18:38:03 +0100

    Add VaultSelector component

---
+++ src/components/VaultSelector/index.tsx
@@ -0,0 +1,44 @@
+import { VaultRegistryVault } from "@polkadot/types/lookup";
+import LabelledSelector from "../LabelledSelector";
+import { h } from "preact";
+
+interface VaultSelectorProps {
+  vaults: VaultRegistryVault[];
+  selectedVault?: VaultRegistryVault;
+  onChange: (vault: VaultRegistryVault) => void;
+}
+
+function VaultSelector(props: VaultSelectorProps): JSX.Element {
+  const { vaults, selectedVault } = props;
+
+  const items = vaults.map((vault) => {
+    return {
+      displayName: vault.id.accountId.toString(),
+      id: vault.id,
+    };
+  });
+
+  const selectedVaultItem = selectedVault
+    ? {
+        displayName: selectedVault.id.accountId.toString(),
+        id: selectedVault.id,
+      }
+    : undefined;
+
+  return (
+    <LabelledSelector
+      items={items}
+      label="Select Vault"
+      onChange={(newItem) => {
+        const newVault = vaults.find((vault) => {
+          return vault.id === newItem.id;
+        });
+        newVault && props.onChange(newVault);
+      }}
+      value={selectedVaultItem}
+      style={{ marginTop: "8px" }}
+    />
+  );
+}
+
+export default VaultSelector;
