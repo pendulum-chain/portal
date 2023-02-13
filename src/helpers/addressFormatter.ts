15c5c4066fc467e55efd936974bd939ca292da96
Marcel Ebert <mail@marcel-ebert.de>
2023-02-13 10:07:32 +0000

    Add function to get the address for a ss58 format

--- src/helpers/addressFormatter.ts
+++ src/helpers/addressFormatter.ts
@@ -1,3 +1,18 @@
+import { Keyring } from "@polkadot/api";
+
+export function getAddressForFormat(
+  address: string,
+  ss58Format: number | string
+) {
+  if (typeof ss58Format === "string") {
+    ss58Format = parseInt(ss58Format, 10);
+  }
+
+  const keyring = new Keyring();
+  const encodedAddress = keyring.encodeAddress(address, ss58Format);
+  return encodedAddress;
+}
+
 const addressFormatter = (address: string, trimLength = 6): string => {
   const addressLength = address.length;
   return (
