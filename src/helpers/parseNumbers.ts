758e6066ab9e30cb0314980bc283afc54921bf2d
Marcel Ebert <mail@marcel-ebert.de>
2023-02-22 19:08:09 +0100

    Add more number helpers

--- src/helpers/parseNumbers.ts
+++ src/helpers/parseNumbers.ts
@@ -1,8 +1,13 @@
 import BigNumber from "big.js";
 
-export const UNIT_PRECISION = 1000000000000;
+// These are the decimals used for the native currency on the Amplitude network
+export const AmplitudeDecimals = 12;
 
-export const AMPLITUDE_DECIMALS = 18;
+// These are the decimals used by the Stellar network
+export const StellarDecimals = 7;
+
+// These are the decimals used by the FixedU128 type
+export const FixedU128Decimals = 18;
 
 // Change the positive exponent to a high value to prevent toString() returning exponential notation
 BigNumber.PE = 100;
@@ -12,15 +17,39 @@ BigNumber.NE = -20;
 // Converts a decimal number to the native representation (a large integer)
 export const decimalToNative = (value: BigNumber | number | string) => {
   const bigIntValue = new BigNumber(value);
-  const multiplier = new BigNumber(10).pow(AMPLITUDE_DECIMALS);
+  const multiplier = new BigNumber(10).pow(AmplitudeDecimals);
 
   return bigIntValue.times(multiplier);
 };
 
+export const decimalToStellarNative = (value: BigNumber | number | string) => {
+  const bigIntValue = new BigNumber(value);
+  const multiplier = new BigNumber(10).pow(StellarDecimals);
+
+  return bigIntValue.times(multiplier);
+};
+
+export const fixedPointToDecimal = (value: BigNumber | number | string) => {
+  const bigIntValue = new BigNumber(value);
+  const divisor = new BigNumber(10).pow(FixedU128Decimals);
+
+  const quotient = bigIntValue.div(divisor);
+
+  return quotient.toNumber();
+};
+
 export const nativeToDecimal = (value: BigNumber | number | string) => {
   const bigIntValue = new BigNumber(value);
-  // const divisor = new BN(UNIT_PRECISION);
-  const divisor = new BigNumber(10).pow(AMPLITUDE_DECIMALS);
+  const divisor = new BigNumber(10).pow(AmplitudeDecimals);
+
+  const quotient = bigIntValue.div(divisor);
+
+  return quotient.toNumber();
+};
+
+export const nativeStellarToDecimal = (value: BigNumber | number | string) => {
+  const bigIntValue = new BigNumber(value);
+  const divisor = new BigNumber(10).pow(StellarDecimals);
 
   const quotient = bigIntValue.div(divisor);
 
