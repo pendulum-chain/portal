fd6f811447287ca9eb103f54cdf85d87faf32520
Nejc <nmursi2@gmail.com>
2023-02-28 21:09:40 +0700

    feat: useful hooks

---
+++ src/hooks/useBoolean.ts
@@ -0,0 +1,23 @@
+import { useCallback, useMemo, useState } from "preact/compat";
+
+export type UseBooleanActions = {
+  setValue: (val: boolean) => void;
+  toggle: () => void;
+  setTrue: () => void;
+  setFalse: () => void;
+};
+export type UseBoolean = [boolean, UseBooleanActions];
+
+export function useBoolean(initial = false): UseBoolean {
+  const [value, setValue] = useState<boolean>(initial);
+  const toggle = useCallback(() => setValue((v) => !v), []);
+  const setTrue = useCallback(() => setValue(true), []);
+  const setFalse = useCallback(() => setValue(false), []);
+  const actions = useMemo(
+    () => ({ setValue, toggle, setTrue, setFalse }),
+    [setFalse, setTrue, toggle]
+  );
+  return useMemo(() => [value, actions], [actions, value]);
+}
+
+export default useBoolean;
