1a38f43222d1afebbc0744398736747f95cd31eb
Gonza Montiel <gon.montiel@gmail.com>
2023-03-06 14:42:21 +0100

    fix claim logic

--- src/pages/collators/dialogs/ClaimRewardsDialog.tsx
+++ src/pages/collators/dialogs/ClaimRewardsDialog.tsx
@@ -2,7 +2,7 @@ import {
   ParachainStakingInflationInflationInfo, useStakingPallet,
 } from "../../../hooks/staking/staking";
 import { useCallback, useMemo, useState } from "preact/hooks";
-import { format } from "../../../helpers/parseNumbers";
+import { format, nativeToDecimal } from "../../../helpers/parseNumbers";
 import { Button, Modal } from "react-daisyui";
 import SuccessDialogIcon from "../../../assets/success-dialog";
 import { CloseButton } from "../../../components/CloseButton";
@@ -12,6 +12,7 @@ import { getErrors } from "../../../helpers/substrate";
 import { toast } from "react-toastify";
 
 interface Props {
+  userRewardsBalance?: string;
   inflationInfo?: ParachainStakingInflationInflationInfo;
   tokenSymbol?: string;
   visible?: boolean;
@@ -26,6 +27,7 @@ enum ClaimStep {
 
 function ClaimRewardsDialog(props: Props) {
   const {
+    userRewardsBalance = "0",
     tokenSymbol,
     visible,
     onClose,
@@ -35,14 +37,13 @@ function ClaimRewardsDialog(props: Props) {
   const { api } = useNodeInfoState().state;
   const { walletAccount } = useGlobalState().state;
   const [loading, setLoading] = useState<boolean>(false);
-  const [amount, setAmount] = useState<string>("0");
   const [step, setStep] = useState<ClaimStep>(ClaimStep.Confirm);
+  const amount = nativeToDecimal(userRewardsBalance);
 
   useMemo(() => {
     if (!visible)
       setTimeout(() => {
         setStep(ClaimStep.Confirm)
-        setAmount("0")
       }, 500)
   }, [visible])
 
@@ -51,7 +52,7 @@ function ClaimRewardsDialog(props: Props) {
     if (!walletAccount || !api || !amount)
       return;
 
-    const extrinsic = createClaimRewardExtrinsic(amount);
+    const extrinsic = createClaimRewardExtrinsic(userRewardsBalance);
 
     extrinsic
       ?.signAndSend(
@@ -87,13 +88,13 @@ function ClaimRewardsDialog(props: Props) {
     walletAccount, setLoading, setStep]);
 
 
-  const getContentForStep = (step: ClaimStep) => {
+  const content = useMemo(() => {
     switch (step) {
       case ClaimStep.Confirm:
         return (
           <div className="rounded-lg bg-base-200 flex flex-col p-8 items-center w-fit center m-auto">
             <p className="flex">Amount</p>
-            <h1 className="flex text-4xl">{format(parseFloat(amount), tokenSymbol, true)}</h1>
+            <h1 className="flex text-4xl">{format(amount, tokenSymbol, true)}</h1>
           </div>
         )
       case ClaimStep.Success:
@@ -106,7 +107,7 @@ function ClaimRewardsDialog(props: Props) {
             </h2>
           </div>)
     }
-  }
+  }, [step]);
 
   const getButtonText = (step: ClaimStep) => {
     switch (step) {
@@ -135,7 +136,7 @@ function ClaimRewardsDialog(props: Props) {
       <CloseButton onClick={onClose} />
       <Modal.Body>
         <div className="mt-4" />
-        {getContentForStep(step)}
+        {content}
       </Modal.Body>
       <Modal.Actions className="justify-center mt-10">
         <Button
@@ -143,7 +144,7 @@ function ClaimRewardsDialog(props: Props) {
           color="primary"
           loading={loading}
           onClick={getButtonAction(step)}
-          disabled={!walletAccount || parseFloat(amount) <= 0}
+          disabled={!walletAccount || amount <= 0}
         >
           {getButtonText(step)}
         </Button>
