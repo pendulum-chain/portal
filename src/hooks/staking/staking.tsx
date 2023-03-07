64e4b5379666f4a3487d1cbdd94f60b39ae3a0c5
Marcel Ebert <mail@marcel-ebert.de>
2023-03-07 10:17:43 +0100

    Add delegation function to collator list (#53)
    * Fix Amplitude not connecting
    
    * Add react-table dependency
    
    * Change collator table
    
    * Add secondaryLabel to input field
    
    * Change PublicKey to support input of any length
    
    * Add an extra check for commas in number conversions
    
    * Fix setting of wrong theme names
    
    * Add hook for staking pallet
    
    * Add component for AmplitudeLogo
    
    * Change Collators view
    
    * Fix fee calculation and add submission of extrinsic
    
    * Extract dialogs
    
    * Add function to get the address for a ss58 format
    
    * Rename function to trim address
    
    * Add UnlinkIcon
    
    * Change address shown in OpenWallet
    
    * Add Unbond button to Collators view
    
    * Add DelegationSuccessfulDialog
    
    * Group dialogs under DelegationDialog
    
    * Merge remote-tracking branch 'origin' into 26-add-delegation
    
    # Conflicts:
    #	package.json
    #	src/GlobalStateProvider.tsx
    #	src/NodeInfoProvider.tsx
    #	src/components/LabelledInputField/index.tsx
    #	src/components/Layout/index.tsx
    #	src/components/OpenWallet.tsx
    #	src/components/PublicKey/index.tsx
    #	src/helpers/parseNumbers.ts
    #	src/index.css
    #	src/pages/collators/Collators.tsx
    #	src/pages/dashboard/Dashboard.tsx
    #	src/pages/dashboard/styles.css
    #	yarn.lock
    
    * added staking rewards icons and styles
    
    * remove quotes from colors
    
    * collators rewards boxes styling
    
    * add user staking totals and rewards
    
    * add extra button (Max) to labelled input
    
    * call delegate more if the account is already delegating to candidate
    
    * break infinite loop
    
    * Add function to automatically add a prefix to large or very tiny numbers (such as mili AMPE, millions AMPE, etc)
    
    * close button component
    
    * fetch staking rewards info
    
    * change name to reflect both extrinsics
    
    * claim reward dialogs
    
    * so, apparently we cannot define the amount to be rewarded, so I had to disable the input and set the max value instead
    
    * remove dependency that was already included in dev dependencies
    
    * social and terms
    
    * update package and lock
    
    * update test
    
    * add fixme and ts ignore beacuse of hard to track errors in react-table
    
    * show buttons logic
    
    * disclaimer for user
    
    * hide min bond when is delegating more
    
    * fix delegate when already delegating
    
    * improve formatting
    
    * remove extra space
    
    * fix indentation
    
    * adopt bg-base* pattern, remove --background
    
    * remove Claim rewards step
    
    * fix claim logic
    
    * fetch delegators apy
    
    * collapse border
    
    ---------
    
    Co-authored-by: Gonza <gonzalo@satoshipay.io>
    Co-authored-by: Gonza Montiel <gon.montiel@gmail.com>

---
+++ src/hooks/staking/staking.tsx
@@ -0,0 +1,184 @@
+import { useEffect, useMemo, useState } from "preact/hooks";
+import Big from "big.js";
+import { useNodeInfoState } from "../../NodeInfoProvider";
+import { SubmittableExtrinsic } from "@polkadot/api/promise/types";
+import { Option, U128 } from "@polkadot/types-codec";
+import { useGlobalState } from "../../GlobalStateProvider";
+import { nativeToDecimal } from "../../helpers/parseNumbers";
+
+interface ParachainStakingDelegator {
+  owner: string;
+  amount: string;
+}
+
+export interface ParachainStakingCandidate {
+  id: string;
+  stake: string;
+  delegators: ParachainStakingDelegator[];
+  total: string;
+  status: string | false;
+}
+
+interface ParachainStakingStakeOption {
+  owner: string;
+  amount: string;
+}
+
+export interface ParachainStakingInflationInflationInfo {
+  collator: {
+    maxRate: string;
+    rewardRate: {
+      annual: string;
+      perBlock: string;
+    };
+  };
+  delegator: {
+    maxRate: string;
+    rewardRate: {
+      annual: string;
+      perBlock: string;
+    };
+  };
+}
+
+export function useStakingPallet() {
+  const { api } = useNodeInfoState().state;
+  const { walletAccount } = useGlobalState().state;
+
+  const [candidates, setCandidates] = useState<ParachainStakingCandidate[]>([]);
+  const [inflationInfo, setInflationInfo] = useState<
+    ParachainStakingInflationInflationInfo | undefined
+  >(undefined);
+  const [minDelegatorStake, setMinDelegatorStake] = useState<string>("0");
+  const [estimatedRewards, setEstimatedRewards] = useState<string>("0");
+  const [joinDelegatorsTransactionFee, setJoinDelegatorsTransactionFee] =
+    useState<Big>(new Big(0));
+
+  useEffect(() => {
+    if (!api) {
+      return;
+    }
+
+    // Check that the pallet is available
+    if (!api.query.parachainStaking) {
+      return;
+    }
+
+    const fetchCandidatePool = async () => {
+      const entries = await api.query.parachainStaking.candidatePool.entries();
+
+      const newCandidates = entries.map(([_, value]) => {
+        const candidate = (value as Option<any>)
+          .unwrap()
+          .toHuman() as ParachainStakingCandidate;
+
+        return candidate;
+      });
+
+      return newCandidates;
+    };
+
+    const fetchInflationInfo = async () => {
+      const inflationInfo = await api.query.parachainStaking.inflationConfig();
+      return inflationInfo.toHuman() as unknown as ParachainStakingInflationInflationInfo;
+    };
+
+    const fetchEstimatedReward = async () => {
+      if (!walletAccount) return "0";
+      return (await api.query.parachainStaking.rewards(walletAccount?.address)).toString();
+    };
+
+    const fetchJoinDelegatorsTransactionFee = async () => {
+      const dummyAddress = "5D4tzEZy9XeNSwsAXgtZrRrs1bTfpPTWGqwb1PwCYjRTKYYS";
+      const sender = dummyAddress;
+      const info = await api.tx.parachainStaking
+        ?.joinDelegators(dummyAddress, "0")
+        .paymentInfo(sender);
+      return new Big(info.partialFee.toString());
+    };
+
+    fetchCandidatePool().then((candidates) => setCandidates(candidates));
+
+    fetchEstimatedReward().then((reward) =>
+      setEstimatedRewards(reward)
+    );
+
+    fetchInflationInfo().then((inflationInfo) =>
+      setInflationInfo(inflationInfo)
+    );
+
+    fetchJoinDelegatorsTransactionFee().then((fee) =>
+      setJoinDelegatorsTransactionFee(fee)
+    );
+
+    if (api.consts.parachainStaking?.minDelegatorStake) {
+      setMinDelegatorStake(
+        (api.consts.parachainStaking.minDelegatorStake.toHuman() as string) ||
+        "0"
+      );
+    }
+  }, [api, walletAccount, walletAccount?.address]);
+
+  const memo = useMemo(() => {
+    return {
+      candidates,
+      inflationInfo,
+      minDelegatorStake,
+      estimatedRewards,
+      joinDelegatorsTransactionFee,
+      async getTransactionFee(extrinsic: SubmittableExtrinsic) {
+        if (!api || !extrinsic.hasPaymentInfo) {
+          return new Big(0);
+        }
+
+        // Can be any address because we don't care about executing it here
+        const dummyAddress = "5D4tzEZy9XeNSwsAXgtZrRrs1bTfpPTWGqwb1PwCYjRTKYYS";
+        const sender = dummyAddress;
+        const info = await extrinsic.paymentInfo(sender);
+
+        return new Big(info.partialFee.toString());
+      },
+      createClaimRewardExtrinsic(claimAmount: string) {
+        if (!api) {
+          return undefined;
+        }
+        return api.tx.parachainStaking?.claimRewards();
+      },
+      createDelegateMoreExtrinsic(
+        collatorAddress: string,
+        moreAmountNative: string
+      ) {
+        if (!api) {
+          return undefined;
+        }
+
+        return api.tx.parachainStaking?.delegatorStakeMore(
+          collatorAddress,
+          moreAmountNative
+        );
+      },
+      createJoinDelegatorsExtrinsic(
+        collatorAddress: string,
+        amountNative: string
+      ) {
+        if (!api) {
+          return undefined;
+        }
+
+        return api.tx.parachainStaking?.joinDelegators(
+          collatorAddress,
+          amountNative
+        );
+      },
+    };
+  }, [
+    api,
+    candidates,
+    inflationInfo,
+    joinDelegatorsTransactionFee,
+    minDelegatorStake,
+    estimatedRewards
+  ]);
+
+  return memo;
+}
---
+++ src/pages/collators/dialogs/ExecuteDelegationDialogs.tsx
@@ -0,0 +1,146 @@
+import {
+  ParachainStakingCandidate,
+  useStakingPallet,
+} from "../../../hooks/staking/staking";
+import { useNodeInfoState } from "../../../NodeInfoProvider";
+import { useGlobalState } from "../../../GlobalStateProvider";
+import { useCallback, useState } from "preact/hooks";
+import { decimalToNative } from "../../../helpers/parseNumbers";
+import { getErrors } from "../../../helpers/substrate";
+import { toast } from "react-toastify";
+import DelegateToCollatorDialog from "./DelegateToCollatorDialog";
+import ConfirmDelegateDialog from "./ConfirmDelegateDialog";
+import DelegationSuccessfulDialog from "./DelegationSuccessfulDialog";
+import { h } from "preact";
+
+interface ExecuteDelegationDialogsProps {
+  userAvailableBalance: string;
+  selectedCandidateForDelegation: ParachainStakingCandidate | undefined;
+  isDelegatingMore: boolean;
+  onClose: () => void;
+}
+
+function ExecuteDelegationDialogs(props: ExecuteDelegationDialogsProps) {
+  const { userAvailableBalance, selectedCandidateForDelegation, isDelegatingMore, onClose } =
+    props;
+
+  const { api, tokenSymbol } = useNodeInfoState().state;
+  const { walletAccount } = useGlobalState().state;
+
+  const {
+    inflationInfo,
+    minDelegatorStake,
+    createJoinDelegatorsExtrinsic,
+    createDelegateMoreExtrinsic,
+    joinDelegatorsTransactionFee,
+  } = useStakingPallet();
+
+  // Holds the amount that is to be delegated to the candidate
+  const [delegationAmount, setDelegationAmount] = useState<string | undefined>(
+    undefined
+  );
+  const [submissionPending, setSubmissionPending] = useState<boolean>(false);
+  const [confirmationDialogVisible, setConfirmationDialogVisible] =
+    useState<boolean>(false);
+
+  const submitDelegationExtrinsic = useCallback(() => {
+    if (
+      !walletAccount ||
+      !api ||
+      !delegationAmount ||
+      !selectedCandidateForDelegation
+    ) {
+      return;
+    }
+
+    const amount = decimalToNative(delegationAmount);
+    const extrinsic = isDelegatingMore ?
+      createDelegateMoreExtrinsic(
+        selectedCandidateForDelegation.id,
+        amount.toString()) :
+      createJoinDelegatorsExtrinsic(
+        selectedCandidateForDelegation.id,
+        amount.toString()
+      );
+
+    setSubmissionPending(true);
+
+    extrinsic
+      ?.signAndSend(
+        walletAccount.address,
+        { signer: walletAccount.signer as any },
+        (result) => {
+          const { status, events } = result;
+
+          const errors = getErrors(events, api);
+          if (status.isInBlock) {
+            if (errors.length > 0) {
+              const errorMessage = `Transaction failed with errors: ${errors.join(
+                "\n"
+              )}`;
+              console.error(errorMessage);
+              toast(errorMessage, { type: "error" });
+            }
+          } else if (status.isFinalized) {
+            setSubmissionPending(false);
+
+            if (errors.length === 0) {
+              setConfirmationDialogVisible(true);
+            }
+          }
+        }
+      )
+      .catch((error) => {
+        console.error("Transaction submission failed", error);
+        toast("Transaction submission failed", { type: "error" });
+        setSubmissionPending(false);
+      });
+  }, [
+    walletAccount, api, delegationAmount,
+    selectedCandidateForDelegation, isDelegatingMore,
+    createJoinDelegatorsExtrinsic, createDelegateMoreExtrinsic
+  ]);
+
+  return (
+    <>
+      <DelegateToCollatorDialog
+        availableBalance={userAvailableBalance}
+        collator={selectedCandidateForDelegation}
+        inflationInfo={inflationInfo}
+        minDelegatorStake={minDelegatorStake}
+        tokenSymbol={tokenSymbol || ""}
+        isDelegatingMore={isDelegatingMore}
+        visible={Boolean(selectedCandidateForDelegation && !delegationAmount)}
+        onClose={onClose}
+        onSubmit={(amount) => setDelegationAmount(amount)}
+      />
+      <ConfirmDelegateDialog
+        delegationAmountDecimal={delegationAmount}
+        availableBalance={userAvailableBalance}
+        transactionFee={joinDelegatorsTransactionFee}
+        submissionPending={submissionPending}
+        onConfirm={submitDelegationExtrinsic}
+        onCancel={() => setDelegationAmount(undefined)}
+        onClose={() => {
+          setDelegationAmount(undefined);
+          onClose();
+        }}
+        tokenSymbol={tokenSymbol || ""}
+        visible={Boolean(selectedCandidateForDelegation && delegationAmount)}
+      />
+      <DelegationSuccessfulDialog
+        visible={confirmationDialogVisible}
+        onClose={() => {
+          setConfirmationDialogVisible(false);
+          onClose();
+        }}
+        onConfirm={() => {
+          setConfirmationDialogVisible(false);
+          onClose();
+        }}
+      />
+    </>
+  );
+}
+
+export default ExecuteDelegationDialogs;
