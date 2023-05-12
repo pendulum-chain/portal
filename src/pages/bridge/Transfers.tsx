87a535ba60fd9da85373b24121eba4554805a5cf
Gonza Montiel <gonzamontiel@users.noreply.github.com>
2023-05-12 10:43:18 +0200

    Fix/137 transfers page doesnt load (#138)
    * update yarn
    
    * decode asset code from spacewal requests

--- src/pages/bridge/Transfers.tsx
+++ src/pages/bridge/Transfers.tsx
@@ -26,6 +26,8 @@ import {
   updatedColumn,
 } from './TransfersColumns';
 import './styles.css';
+import { SpacewalkPrimitivesCurrencyId } from '@polkadot/types/lookup';
+import { decodeAssetCode } from './helpers';
 
 function Transfers(): JSX.Element {
   const { getIssueRequests } = useIssuePallet();
@@ -62,7 +64,7 @@ function Transfers(): JSX.Element {
         entries.push({
           updated: estimateRequestCreationTime(activeBlockNumber as number, e.request.opentime.toNumber()),
           amount: nativeToDecimal(e.request.amount.toString()).toString(),
-          asset: e.request.asset.asStellar.asAlphaNum4.code.toHuman()?.toString(),
+          asset: decodeAssetCode(e.request.asset),
           transactionId: e.id.toString(),
           type: TransferType.issue,
           status: timedOut ? 'Cancelled' : e.request.status.type,
@@ -74,7 +76,7 @@ function Transfers(): JSX.Element {
         entries.push({
           updated: estimateRequestCreationTime(activeBlockNumber as number, e.request.opentime.toNumber()),
           amount: nativeToDecimal(e.request.amount.toString()).toString(),
-          asset: e.request.asset.asStellar.asAlphaNum4.code.toHuman()?.toString(),
+          asset: decodeAssetCode(e.request.asset),
           transactionId: e.id.toString(),
           type: TransferType.redeem,
           status: e.request.status.type,
