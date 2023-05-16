1dbf28fd59e7f55599ecc7d66dbbc84b441ba189
Gonza Montiel <gon.montiel@gmail.com>
2023-05-16 11:56:02 +0200

    fix redirect

--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -34,7 +34,7 @@ function Dashboard() {
   return (
     <div className="mt-10">
       <div className="card card-compact w-2/3 banner rounded-md mb-6 bg-base-200">
-        <a target="blank" href="https://pendulumchain.org/">
+        <a target="blank" href="https://pendulumchain.org/spacewalk">
           <div className="card-body">
             <div className="card-title block">
               <h2 className={'float-left'}>Promo</h2>
