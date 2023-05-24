0b2cc3e3603e553727b3318d017765999ddbdd04
Marcel Ebert <mail@marcel-ebert.de>
2023-05-24 17:34:11 +0200

    Fix wrong validation

--- src/pages/bridge/Issue.tsx
+++ src/pages/bridge/Issue.tsx
@@ -392,8 +392,8 @@ function Issue(props: IssueProps): JSX.Element {
                   if (!isCompatibleStellarAmount(value)) {
                     return 'Max 7 decimals';
                   }
-                  if (parseFloat(value) > parseFloat(selectedVault?.redeemableTokens?.toString() || '0')) {
-                    return 'Amount is higher than the vault can redeem.';
+                  if (parseFloat(value) > parseFloat(selectedVault?.issuableTokens?.toString() || '0')) {
+                    return 'Amount is higher than the vault can issue.';
                   }
                 },
               }}
