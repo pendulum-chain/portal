3bc2c283cd0c9e49eb0798b978165c72f774d11e
Nejc <nmursi2@gmail.com>
2023-06-21 22:22:06 -0700

    Update src/components/Wallet/WalletConnect/index.tsx
    Co-authored-by: Marcel Ebert <mail@marcel-ebert.de>

--- src/components/Wallet/WalletConnect/index.tsx
+++ src/components/Wallet/WalletConnect/index.tsx
@@ -15,7 +15,10 @@ const wcParams = {
     polkadot: {
       methods: ['polkadot_signTransaction', 'polkadot_signMessage'],
       chains: [
-        'polkadot:91b171bb158e2d3848fa23a9f1c25182', // polkadot
+        'polkadot:67221cd96c1551b72d55f65164d6a39f', // foucoco,
+        'polkadot:cceae7f3b9947cdb67369c026ef78efa', // amplitude
+        'polkadot:5d3c298622d5634ed019bf61ea4b7165', // pendulum
+        
       ],
       events: ['chainChanged", "accountsChanged'],
     },
