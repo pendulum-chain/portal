0b4945df4b6cab2f749eb367ac4d08ba5a23890a
Nejc <nmursi2@gmail.com>
2023-06-22 18:11:49 +0800

    fix: add optional chains

--- src/components/Wallet/WalletConnect/index.tsx
+++ src/components/Wallet/WalletConnect/index.tsx
@@ -14,13 +14,21 @@ const wcParams = {
   requiredNamespaces: {
     polkadot: {
       methods: ['polkadot_signTransaction', 'polkadot_signMessage'],
+      events: ['chainChanged", "accountsChanged'],
+      chains: [
+        'polkadot:91b171bb158e2d3848fa23a9f1c25182', // polkadot
+      ],
+    },
+  },
+  optionalNamespaces: {
+    polkadot: {
+      methods: ['polkadot_signTransaction', 'polkadot_signMessage'],
+      events: ['chainChanged", "accountsChanged'],
       chains: [
         'polkadot:67221cd96c1551b72d55f65164d6a39f', // foucoco,
         'polkadot:cceae7f3b9947cdb67369c026ef78efa', // amplitude
         'polkadot:5d3c298622d5634ed019bf61ea4b7165', // pendulum
-        
       ],
-      events: ['chainChanged", "accountsChanged'],
     },
   },
 };
