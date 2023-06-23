f30ce15d31117b16795c358791ae4bc965c757de
Nejc <nmursi2@gmail.com>
2023-06-23 13:52:49 +0800

    fix: config bug

--- src/config/walletConnect.ts
+++ src/config/walletConnect.ts
@@ -2,7 +2,7 @@ export const walletConnectConfig = {
   requiredNamespaces: {
     polkadot: {
       methods: ['polkadot_signTransaction', 'polkadot_signMessage'],
-      events: ['chainChanged", "accountsChanged'],
+      events: ['chainChanged', 'accountsChanged'],
       chains: [
         'polkadot:91b171bb158e2d3848fa23a9f1c25182', // polkadot
       ],
@@ -11,7 +11,7 @@ export const walletConnectConfig = {
   optionalNamespaces: {
     polkadot: {
       methods: ['polkadot_signTransaction', 'polkadot_signMessage'],
-      events: ['chainChanged", "accountsChanged'],
+      events: ['chainChanged', 'accountsChanged'],
       chains: [
         'polkadot:67221cd96c1551b72d55f65164d6a39f', // foucoco,
         'polkadot:cceae7f3b9947cdb67369c026ef78efa', // amplitude
