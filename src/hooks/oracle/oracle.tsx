f4f7936572052fd209f229d7f4f2a34b6fbe259b
Gonza Montiel <gon.montiel@gmail.com>
2023-07-13 17:07:56 +0200

    fix exchange rate calculation

--- src/hooks/oracle/oracle.tsx
+++ src/hooks/oracle/oracle.tsx
@@ -1,7 +1,10 @@
 import { useMemo } from 'react';
 import { useNodeInfoState } from '../../NodeInfoProvider';
 import { Balance } from '@polkadot/types/interfaces';
-import { SpacewalkPrimitivesCurrencyId } from '@pendulum-chain/types/interfaces';
+import { BalanceWrapper, SpacewalkPrimitivesCurrencyId } from '@pendulum-chain/types/interfaces';
+import { AbstractInt, U128, UInt, u128 } from '@polkadot/types-codec';
+import { decimalToNative } from '../../helpers/parseNumbers';
+import { BN } from '@polkadot/util';
 
 interface CurrencyId extends SpacewalkPrimitivesCurrencyId {}
 
@@ -12,7 +15,7 @@ export function useOraclePallet() {
     return {
       async currencyToUsd(amount: Balance, currency: CurrencyId) {
         if (!api) return;
-        return await api.rpc.oracle.currencyToUsd(toString(), currency);
+        return await api.rpc.oracle.currencyToUsd(amount.toString(), currency);
       },
       async usdToCurrency(amount: Balance, currency: CurrencyId) {
         if (!api) return;
@@ -20,10 +23,16 @@ export function useOraclePallet() {
       },
       async getExchangeRate(currency_a: CurrencyId, currency_b: CurrencyId): Promise<number> {
         if (!api) return 0;
-        const currency_a_usd = await api.rpc.oracle.currencyToUsd('1', currency_a);
-        const currency_b_usd = await api.rpc.oracle.currencyToUsd('1', currency_b);
-        const rate = currency_a_usd.amount.div(currency_b_usd.amount);
-        return rate.toNumber();
+        const balance: Balance = new UInt(api.registry, '100000');
+        try {
+          const currency_a_usd = await api.rpc.oracle.currencyToUsd({ amount: balance } as BalanceWrapper, currency_a);
+          const currency_b_usd = await api.rpc.oracle.currencyToUsd({ amount: balance } as BalanceWrapper, currency_b);
+          const rate = currency_a_usd.amount.div(currency_b_usd.amount);
+          return rate.toNumber();
+        } catch (e) {
+          console.log(e);
+          return 0;
+        }
       },
     };
   }, [api]);
