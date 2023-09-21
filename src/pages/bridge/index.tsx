43d8f208d584bfd8584eaf2fb102ba6939a8a242
Gonza Montiel <gon.montiel@gmail.com>
2023-09-21 14:47:02 +0200

    add stellar logo for tab

--- src/pages/bridge/index.tsx
+++ src/pages/bridge/index.tsx
@@ -3,6 +3,7 @@ import { Card, Tabs } from 'react-daisyui';
 import { useNodeInfoState } from '../../NodeInfoProvider';
 import AmplitudeLogo from '../../assets/AmplitudeLogo';
 import PendulumLogo from '../../assets/PendulumLogo';
+import StellarLogo from '../../assets/StellarLogo';
 import { SpacewalkConstants } from '../../helpers/spacewalk';
 import Issue from './Issue';
 import Redeem from './Redeem';
@@ -26,15 +27,16 @@ function Bridge(): JSX.Element | null {
 
   return chain ? (
     <div className="h-full flex items-center justify-center mt-4">
-      <Card className="bridge-card min-h-500 w-full max-w-[520px]">
+      <Card className="bridge-card bg-base-200 min-h-500 w-full max-w-[520px] rounded-lg">
         <div className="flex justify-between px-4 xs:px-10 mt-5">
           <Tabs className="flex flex-grow" boxed value={tabValue} onChange={setTabValue}>
             <Tabs.Tab className="w-2/4 h-fit p-2" value={0}>
-              {chain === 'Pendulum' && <PendulumLogo className="w-6 h-6" />}
-              {chain === 'Amplitude' && <AmplitudeLogo className="w-6 h-6" />}
+              {chain === 'Pendulum' && <PendulumLogo className="w-6 h-6 mr-1" />}
+              {chain === 'Amplitude' && <AmplitudeLogo className="w-6 h-6 mr-1" />}
               To {chain}
             </Tabs.Tab>
             <Tabs.Tab className="w-2/4 h-fit p-2" value={1}>
+              <StellarLogo className="w-6 h-6 mr-1" />
               Back To Stellar
             </Tabs.Tab>
           </Tabs>
