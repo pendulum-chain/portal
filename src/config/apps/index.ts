b5547fb85b8e690d5f94dda28ec84268cedcaa16
Gonza Montiel <gon.montiel@gmail.com>
2023-09-28 11:25:03 +0200

    remove unused spacewalk app

--- src/config/apps/index.ts
+++ src/config/apps/index.ts
@@ -1,13 +1,11 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { nablaConfig } from './nabla';
-import { spacewalkConfig } from './spacewalk';
 import { AppConfig } from './types';
 
-export type Apps = 'nabla' | 'spacewalk';
+export type Apps = 'nabla';
 
 export const appsConfigs = {
   nabla: nablaConfig,
-  spacewalk: spacewalkConfig,
 } satisfies Record<Apps, AppConfig>;
 
 export type AppsConfig = typeof appsConfigs;
