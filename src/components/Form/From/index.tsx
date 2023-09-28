4dcdcd4a39015a9852de2ec36d2e92a63037e922
Gonza Montiel <gon.montiel@gmail.com>
2023-09-28 16:22:30 +0200

    invert buttons order

--- src/components/Form/From/index.tsx
+++ src/components/Form/From/index.tsx
@@ -64,15 +64,15 @@ const From = ({
             {max !== undefined && (
               <Fragment>
                 <span className="mr-1">Available: {max.toFixed(2)}</span>
-                <button className="text-primary hover:underline" onClick={() => setValue(Number(max))} type="button">
-                  MAX
-                </button>
                 <button
                   className="text-primary hover:underline"
                   onClick={() => setValue(Number(max) * 0.5)}
                   type="button"
                 >
                   50%
+                </button>
+                <button className="text-primary hover:underline" onClick={() => setValue(Number(max))} type="button">
+                  MAX
                 </button>
               </Fragment>
             )}
