ab9c3e2c41ace9705ebec0faf08627bcf2bee71b
Gonza Montiel <gon.montiel@gmail.com>
2023-11-15 00:11:59 +0100

    update cancelled dialog

--- src/pages/bridge/TransferDialog.tsx
+++ src/pages/bridge/TransferDialog.tsx
@@ -185,18 +185,19 @@ export function CompletedTransferDialog(props: TransferDialogProps) {
 
 export function CancelledTransferDialog(props: TransferDialogProps) {
   const { transfer, visible, onClose } = props;
+  const { tenantName } = useGlobalState();
   const stellarAsset = convertCurrencyToStellarAsset(transfer.original.asset)?.getCode();
   const amountToSend = nativeToDecimal(transfer.original.amount.add(transfer.original.fee).toNumber()).toNumber();
   const content = (
     <>
-      <div className="text-sm">
+      <div className="text-md p-5 text-[#7C818D] align-middle text-center">
         {`You did not send a Stellar transaction in time, or the transferred amount did not meet the requested amount of ${amountToSend} 
           ${stellarAsset}.`}
       </div>
-      <div className="mt-4" />
-      <div className="text-sm text-accent ">Contact the team for debugging if you think this is an error.</div>
-      <Divider />
-      <div className="mt-5" />
+      <div className="text-md text-[#907EA0]">Contact the team for debugging if you think this is an error.</div>
+      <label className="rounded bg-black bg-opacity-3 px-4 py-2 my-4 text font-semibold text-black">
+        {`To ${toTitle(tenantName)}`}
+      </label>
       <div className="flex flex-row justify-between w-11/12">
         <div className="text-xs">Spacewalk transaction</div>
         <CopyableAddress inline={true} className="text-xs" variant="hexa" publicKey={transfer.transactionId} />
