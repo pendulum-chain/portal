5c8e71732c90986340c58240bcb6c475ad4191d0
Gonza Montiel <gon.montiel@gmail.com>
2023-11-14 14:18:02 +0100

    general dialogs improvements

--- src/pages/bridge/TransferDialog.tsx
+++ src/pages/bridge/TransferDialog.tsx
@@ -1,6 +1,6 @@
 import { hexToU8a } from '@polkadot/util';
 import { DateTime } from 'luxon';
-import { useEffect, useMemo, useState } from 'preact/compat';
+import { useCallback, useEffect, useMemo, useState } from 'preact/compat';
 import { JSXInternal } from 'preact/src/jsx';
 import { Button, Divider, Modal } from 'react-daisyui';
 import { useGlobalState } from '../../GlobalStateProvider';
@@ -33,12 +33,6 @@ interface BaseTransferDialogProps {
   onConfirm?: () => void;
 }
 
-const defaultActions = (onConfirm: (() => void) | undefined) => (
-  <Button className="px-6" color="primary" onClick={onConfirm}>
-    OK
-  </Button>
-);
-
 function BaseTransferDialog(props: BaseTransferDialogProps) {
   const { id, statusIcon, showMemo, transfer, visible, title, content, footer, actions, onClose, onConfirm } = props;
 
@@ -46,6 +40,19 @@ function BaseTransferDialog(props: BaseTransferDialogProps) {
   const tenantNameCapitalized = tenantName ? toTitle(tenantName) : 'Pendulum';
 
   const [vaultStellarPublicKey, setVaultStellarPublicKey] = useState<string | undefined>(undefined);
+  const [collapseVisibility, setCollapseVisibility] = useState('');
+
+  const toggle = useCallback(() => {
+    if (collapseVisibility === '') {
+      setCollapseVisibility('collapse-open');
+    } else {
+      setCollapseVisibility('');
+      const elem = document.activeElement;
+      if (elem && elem instanceof HTMLElement) {
+        elem.blur();
+      }
+    }
+  }, [collapseVisibility, setCollapseVisibility]);
 
   // The `stellarAddress` contained in the request will either be the user's Stellar address or the vault's Stellar address (i.e. equal to `vaultStellarPublicKey`).
   const destinationStellarAddress = convertRawHexKeyToPublicKey(transfer.original.stellarAddress.toHex()).publicKey();
@@ -69,66 +76,72 @@ function BaseTransferDialog(props: BaseTransferDialogProps) {
   }, [transfer]);
 
   return (
-    <Modal id={id} open={visible} className="bg-base-200">
+    <Modal id={id} open={visible} className="rounded-md bg-base-200 px-2">
       <CloseButton onClick={onClose} />
       <Modal.Body>
         <div className="flex flex-col items-center justify-between">
           {statusIcon}
-          <div className="mt-4" />
-          <h1 className="text-xl mb-5">{title}</h1>
+          <div className="mt-5" />
+          <h1 className="text-2xl text-black font-semibold mb-1">{title}</h1>
           {content}
-          <div id="details" className="rounded flex flex-col p-5 mt-1 w-11/12">
-            <div className="flex flex-row justify-between">
+          <Divider className="mx-5 mb-2 mt-1" />
+          <div
+            id="details"
+            tabIndex={0}
+            onClick={toggle}
+            className={`collapse collapse-arrow rounded-lg bg-black bg-opacity-3 text-[#7C818D] flex flex-col w-11/12 ${collapseVisibility}`}
+          >
+            <div className="collapse-title flex flex-row justify-between">
               <div className="text-sm">Bridge fee</div>
               <div className="text-sm">{nativeToDecimal(transfer.original.fee.toNumber()).toString()}</div>
             </div>
-            <div className="flex flex-row justify-between">
-              <div className="text-sm">Destination Address (Stellar)</div>
-              <CopyableAddress
-                inline={true}
-                className="text-sm p0"
-                variant="short"
-                publicKey={destinationStellarAddress}
-              />
-            </div>
-            <div className="flex flex-row justify-between">
-              <div className="text-sm">Vault Address ({tenantNameCapitalized})</div>
-              <CopyableAddress
-                inline={true}
-                className="text-sm p-0"
-                variant="short"
-                publicKey={transfer.original.vault.accountId.toString()}
-              />
-            </div>
-            {vaultStellarPublicKey && (
+            <div className="collapse-content space-y-4">
               <div className="flex flex-row justify-between">
-                <div className="text-sm">Vault Address (Stellar)</div>
+                <div className="text-sm">Destination Address (Stellar)</div>
+                <CopyableAddress
+                  inline={true}
+                  className="text-sm p0"
+                  variant="short"
+                  publicKey={destinationStellarAddress}
+                />
+              </div>
+              <div className="flex flex-row justify-between">
+                <div className="text-sm">Vault PEN Address ({tenantNameCapitalized})</div>
                 <CopyableAddress
                   inline={true}
                   className="text-sm p-0"
                   variant="short"
-                  publicKey={vaultStellarPublicKey}
+                  publicKey={transfer.original.vault.accountId.toString()}
                 />
               </div>
-            )}
-            {showMemo && (
-              <div className="flex flex-row justify-between">
-                <div className="text-sm">Memo</div>
-                <CopyableAddress
-                  inline={true}
-                  className="text-sm p-0"
-                  variant="short"
-                  publicKey={expectedStellarMemo}
-                />
-              </div>
-            )}
+              {vaultStellarPublicKey && (
+                <div className="flex flex-row justify-between">
+                  <div className="text-sm">Vault PEN Address</div>
+                  <CopyableAddress
+                    inline={true}
+                    className="text-sm p-0"
+                    variant="short"
+                    publicKey={vaultStellarPublicKey}
+                  />
+                </div>
+              )}
+              {showMemo && (
+                <div className="flex flex-row justify-between">
+                  <div className="text-sm">Memo</div>
+                  <CopyableAddress
+                    inline={true}
+                    className="text-sm p-0"
+                    variant="short"
+                    publicKey={expectedStellarMemo}
+                  />
+                </div>
+              )}
+            </div>
           </div>
         </div>
         {footer}
       </Modal.Body>
-      <Modal.Actions className="justify-center">
-        {actions ? actions(onConfirm) : defaultActions(onConfirm)}
-      </Modal.Actions>
+      <Modal.Actions className="justify-center">{actions && actions(onConfirm)}</Modal.Actions>
     </Modal>
   );
 }
