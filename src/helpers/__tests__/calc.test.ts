d821fd74ec1f39544a2459f60214ec7570d96e36
Nejc <nmursi2@gmail.com>
2024-01-08 20:35:57 +0100

    test: fix

--- src/helpers/__tests__/calc.test.ts
+++ src/helpers/__tests__/calc.test.ts
@@ -3,18 +3,20 @@ import { defaultDecimals } from '../../config/apps/nabla';
 import { decimalToNative } from '../../shared/parseNumbers';
 import * as helpers from '../calc';
 
+const native1000 = decimalToNative(1000, defaultDecimals).toString();
+
 describe('calc', () => {
   it('should return correct withdraw amount from calcAvailablePoolWithdraw', () => {
     expect(
       helpers.calcAvailablePoolWithdraw({
         selectedPool: {
           liabilities: undefined,
-          reserves: '1000000000000000000000', // 1000
+          reserves: native1000,
         },
-        shares: BigInt('1000000000000000000000'), // 1000
-        deposit: BigInt('1000000000000000000000'), // 1000
-        bpPrice: BigInt('100000000'), // 1
-        spPrice: BigInt('200000000'), // 2
+        shares: BigInt(native1000),
+        deposit: BigInt(native1000),
+        bpPrice: BigInt(decimalToNative(1, defaultDecimals).toString()),
+        spPrice: BigInt(decimalToNative(2, defaultDecimals).toString()),
         decimals: defaultDecimals,
       }),
     ).toBe(undefined);
@@ -22,13 +24,13 @@ describe('calc', () => {
     expect(
       helpers.calcAvailablePoolWithdraw({
         selectedPool: {
-          liabilities: '1000000000000000000000', // 1000
-          reserves: '1000000000000000000000', // 1000
+          liabilities: native1000,
+          reserves: native1000,
         },
-        shares: BigInt('1000000000000000000000'), // 1000
-        deposit: BigInt('1000000000000000000000'), // 1000
-        bpPrice: BigInt('100000000'), // 1
-        spPrice: BigInt('200000000'), // 2
+        shares: BigInt(native1000),
+        deposit: BigInt(native1000),
+        bpPrice: BigInt(decimalToNative(1, defaultDecimals).toString()),
+        spPrice: BigInt(decimalToNative(2, defaultDecimals).toString()),
         decimals: defaultDecimals,
       }),
     ).toEqual(Big(0));
@@ -36,29 +38,29 @@ describe('calc', () => {
     expect(
       helpers.calcAvailablePoolWithdraw({
         selectedPool: {
-          liabilities: '1000000000000000000000',
-          reserves: '1200000000000000000000', // 1200
+          liabilities: native1000,
+          reserves: decimalToNative(1200, defaultDecimals).toString(),
         },
-        shares: BigInt('1000000000000000000000'), // 1000
-        deposit: BigInt('1000000000000000000000'), // 1000
-        bpPrice: BigInt('110000000'), // 1.1
-        spPrice: BigInt('220000000'), // 2.2
+        shares: BigInt(native1000),
+        deposit: BigInt(native1000),
+        bpPrice: BigInt(decimalToNative(1.1, defaultDecimals).toString()),
+        spPrice: BigInt(decimalToNative(2.2, defaultDecimals).toString()),
         decimals: defaultDecimals,
       }),
-    ).toEqual(decimalToNative('400', defaultDecimals));
+    ).toEqual(decimalToNative(400, defaultDecimals));
 
     expect(
       helpers.calcAvailablePoolWithdraw({
         selectedPool: {
-          liabilities: '1000000000000000000000',
-          reserves: '5200000000000000000000', // 5200
+          liabilities: native1000,
+          reserves: decimalToNative(5200, defaultDecimals).toString(),
         },
-        shares: BigInt('1000000000000000000000'), // 1000
-        deposit: BigInt('1000000000000000000000'), // 1000
-        bpPrice: BigInt('110000000'), // 1.1
-        spPrice: BigInt('220000000'), // 2.2
+        shares: BigInt(native1000),
+        deposit: BigInt(native1000),
+        bpPrice: BigInt(decimalToNative(1.1, defaultDecimals).toString()),
+        spPrice: BigInt(decimalToNative(2.2, defaultDecimals).toString()),
         decimals: defaultDecimals,
       }),
-    ).toEqual(decimalToNative('1000', defaultDecimals));
+    ).toEqual(decimalToNative(1000, defaultDecimals));
   });
 });
