2ebc5d07c9e2d9e50a385b7983dcbcbc95bbfa25
Gonza Montiel <gonzamontiel@users.noreply.github.com>
2024-02-14 15:14:12 -0300

    feat: Update designs for Updating and Claiming staking rewards (#336)
    * staking rewards design changes
    
    * use our Amount component for claiming rewards
    
    * color tweaks for amplitude

--- src/pages/collators/dialogs/ClaimRewardsDialog.tsx
+++ src/pages/collators/dialogs/ClaimRewardsDialog.tsx
@@ -1,13 +1,17 @@
+import { yupResolver } from '@hookform/resolvers/yup';
 import { useCallback, useMemo, useState } from 'preact/hooks';
 import { Button, Modal } from 'react-daisyui';
+import { useForm } from 'react-hook-form';
 import { toast } from 'react-toastify';
 import { useGlobalState } from '../../../GlobalStateProvider';
 import { useNodeInfoState } from '../../../NodeInfoProvider';
 import SuccessDialogIcon from '../../../assets/dialog-status-success';
 import { CloseButton } from '../../../components/CloseButton';
+import Amount from '../../../components/Form/Amount';
 import { getErrors } from '../../../helpers/substrate';
 import { ParachainStakingInflationInflationInfo, useStakingPallet } from '../../../hooks/staking/staking';
-import { format, nativeToDecimal } from '../../../shared/parseNumbers';
+import { nativeToDecimal } from '../../../shared/parseNumbers';
+import { getClaimingValidationSchema } from './ValidationSchema';
 
 interface Props {
   userRewardsBalance?: string;
@@ -23,6 +27,10 @@ enum ClaimStep {
   Success = 1,
 }
 
+export type ClaimFormValues = {
+  amount: number;
+};
+
 function ClaimRewardsDialog(props: Props) {
   const { userRewardsBalance = '0', tokenSymbol, visible, onClose } = props;
   const { createClaimRewardExtrinsic } = useStakingPallet();
@@ -30,7 +38,16 @@ function ClaimRewardsDialog(props: Props) {
   const { walletAccount } = useGlobalState();
   const [loading, setLoading] = useState<boolean>(false);
   const [step, setStep] = useState<ClaimStep>(ClaimStep.Confirm);
-  const amount = nativeToDecimal(userRewardsBalance);
+  const balance = nativeToDecimal(userRewardsBalance).toNumber();
+
+  const form = useForm<ClaimFormValues>({
+    resolver: yupResolver(getClaimingValidationSchema(balance)),
+    defaultValues: {
+      amount: parseFloat(balance.toFixed(2)),
+    },
+  });
+
+  const { handleSubmit, formState, register, setValue, getValues, control } = form;
 
   useMemo(() => {
     if (!visible)
@@ -39,46 +56,55 @@ function ClaimRewardsDialog(props: Props) {
       }, 500);
   }, [visible]);
 
-  const submitExtrinsic = useCallback(() => {
-    if (!walletAccount || !api || !amount) return;
+  const submitExtrinsic = useCallback(
+    (selectedAmount: number) => {
+      if (!walletAccount || !api || formState.errors.amount) return;
 
-    setLoading(true);
+      setLoading(true);
 
-    const extrinsic = createClaimRewardExtrinsic(userRewardsBalance);
+      const extrinsic = createClaimRewardExtrinsic(selectedAmount.toString());
 
-    extrinsic
-      // eslint-disable-next-line @typescript-eslint/no-explicit-any
-      ?.signAndSend(walletAccount.address, { signer: walletAccount.signer as any }, (result) => {
-        const { status, events } = result;
+      extrinsic
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        ?.signAndSend(walletAccount.address, { signer: walletAccount.signer as any }, (result) => {
+          const { status, events } = result;
 
-        const errors = getErrors(events, api);
-        if (status.isInBlock) {
-          if (errors.length > 0) {
-            const errorMessage = `Transaction failed with errors: ${errors.join('\n')}`;
-            console.error(errorMessage);
-            toast(errorMessage, { type: 'error' });
+          const errors = getErrors(events, api);
+          if (status.isInBlock) {
+            if (errors.length > 0) {
+              const errorMessage = `Transaction failed with errors: ${errors.join('\n')}`;
+              console.error(errorMessage);
+              toast(errorMessage, { type: 'error' });
+            }
+          } else if (status.isFinalized) {
+            setLoading(false);
+            if (errors.length === 0) {
+              setStep(ClaimStep.Success);
+            }
           }
-        } else if (status.isFinalized) {
+        })
+        .catch((error) => {
+          console.error('Transaction submission failed', error);
+          toast('Transaction submission failed', { type: 'error' });
           setLoading(false);
-          if (errors.length === 0) {
-            setStep(ClaimStep.Success);
-          }
-        }
-      })
-      .catch((error) => {
-        console.error('Transaction submission failed', error);
-        toast('Transaction submission failed', { type: 'error' });
-        setLoading(false);
-      });
-  }, [api, amount, createClaimRewardExtrinsic, walletAccount, setLoading, setStep, userRewardsBalance]);
+        });
+    },
+    [api, formState, createClaimRewardExtrinsic, walletAccount, setLoading, setStep, userRewardsBalance],
+  );
 
   const content = useMemo(() => {
     switch (step) {
       case ClaimStep.Confirm:
         return (
-          <div className="rounded-lg bg-base-200 flex flex-col p-8 items-center w-fit center m-auto">
-            <p className="flex">Amount</p>
-            <h1 className="flex text-4xl">{format(amount.toNumber(), tokenSymbol)}</h1>
+          <div className="rounded-lg flex flex-col items-center w-full">
+            <form className="flex flex-col">
+              <Amount
+                register={register('amount')}
+                max={nativeToDecimal(userRewardsBalance).toNumber()}
+                setValue={(n: number) => setValue('amount', n)}
+                error={formState.errors.amount?.message?.toString()}
+              />
+            </form>
           </div>
         );
       case ClaimStep.Success:
@@ -90,7 +116,7 @@ function ClaimRewardsDialog(props: Props) {
           </div>
         );
     }
-  }, [amount, step, tokenSymbol]);
+  }, [step, tokenSymbol, formState]);
 
   const getButtonText = (step: ClaimStep) => {
     switch (step) {
@@ -105,7 +131,7 @@ function ClaimRewardsDialog(props: Props) {
     switch (step) {
       case ClaimStep.Confirm:
         return () => {
-          submitExtrinsic();
+          submitExtrinsic(getValues().amount);
         };
       case ClaimStep.Success:
         return onClose;
@@ -113,21 +139,12 @@ function ClaimRewardsDialog(props: Props) {
   };
 
   return (
-    <Modal open={visible}>
-      <Modal.Header className="font-bold">Claim Rewards</Modal.Header>
+    <Modal className="bg-base-200" open={visible}>
+      <Modal.Header className="text-2xl claim-title">Claim Rewards</Modal.Header>
       <CloseButton onClick={onClose} />
-      <Modal.Body>
-        <div className="mt-4" />
-        {content}
-      </Modal.Body>
-      <Modal.Actions className="justify-center mt-10">
-        <Button
-          className="px-12 text-thin"
-          color="primary"
-          loading={loading}
-          onClick={getButtonAction(step)}
-          disabled={!walletAccount || amount.lt(0)}
-        >
+      <Modal.Body>{content}</Modal.Body>
+      <Modal.Actions className="justify-center">
+        <Button color="primary" loading={loading} onClick={getButtonAction(step)} disabled={!walletAccount}>
           {getButtonText(step)}
         </Button>
       </Modal.Actions>
--- src/pages/collators/dialogs/ValidationSchema.ts
+++ src/pages/collators/dialogs/ValidationSchema.ts
@@ -15,3 +15,14 @@ export function getStakingValidationSchema(max: number) {
       .max(max, 'Amount is higher than available.'),
   });
 }
+
+export function getClaimingValidationSchema(max: number) {
+  return Yup.object<FormValues>().shape({
+    amount: Yup.number()
+      .typeError('Value is invalid.')
+      .transform(transformNumber)
+      .positive('You need to enter a positive number.')
+      .required('This field is required.')
+      .max(max, 'Amount is higher than available.'),
+  });
+}
