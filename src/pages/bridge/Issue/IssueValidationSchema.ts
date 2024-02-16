cbca70f2550a462477222156c465fe1e5fa7316e
Gonza Montiel <gon.montiel@gmail.com>
2024-02-16 17:17:45 -0300

    Add security deposit validation when issuing

--- src/pages/bridge/Issue/IssueValidationSchema.ts
+++ src/pages/bridge/Issue/IssueValidationSchema.ts
@@ -2,7 +2,7 @@ import * as Yup from 'yup';
 import { IssueFormValues } from '.';
 import { transformNumber } from '../../../helpers/yup';
 
-export function getIssueValidationSchema(maxIssuable: number) {
+export function getIssueValidationSchema(maxIssuable: number, balance: number) {
   return Yup.object<IssueFormValues>().shape({
     amount: Yup.number()
       .typeError('Value is invalid.')
@@ -10,5 +10,8 @@ export function getIssueValidationSchema(maxIssuable: number) {
       .positive('You need to enter a positive number.')
       .required('This field is required.')
       .max(maxIssuable, 'The vault cannot issue that amount of the selected asset.'),
+    securityDeposit: Yup.number()
+      .transform(transformNumber)
+      .max(balance, "You don't have enough balance to pay the deposit fees."),
   });
 }
