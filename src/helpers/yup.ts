13fa6cacb713985557af21713f2f37033ac787cc
Gonza Montiel <gon.montiel@gmail.com>
2024-02-20 14:55:27 -0300

    change any to unknown for unknown values

--- src/helpers/yup.ts
+++ src/helpers/yup.ts
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-export const transformNumber = (value: any, originalValue: any) => {
+export const transformNumber = (value: unknown, originalValue: any) => {
   if (!originalValue) return 0;
   if (typeof originalValue === 'string' && originalValue !== '') value = Number(originalValue) ?? 0;
   return value;
