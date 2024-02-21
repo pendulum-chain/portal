9f9b7decac1cb190c0858891ad5b9ce7f7c6718f
Kacper Szarkiewicz <szarkiewiczmail@gmail.com>
2024-02-21 15:18:10 +0100

    separate node info fetching function into smaller functions

--- src/NodeInfoProvider.tsx
+++ src/NodeInfoProvider.tsx
@@ -5,6 +5,17 @@ import { createContext } from 'preact';
 import { useContext, useEffect, useState } from 'preact/hooks';
 import { toast } from 'react-toastify';
 
+async function createApiPromise(provider: WsProvider) {
+  return ApiPromise.create(
+    options({
+      provider,
+      rpc,
+      throwOnConnect: true,
+      throwOnUnknown: true,
+    }),
+  );
+}
+
 export interface NodeInfoProviderInterface {
   bestNumberFinalize?: number;
   chain?: string;
@@ -21,18 +32,57 @@ const NodeInfoContext = createContext({
   setState: {} as Dispatch<SetStateAction<Partial<NodeInfoProviderInterface>>>,
 });
 
-const NodeInfoProvider = ({
-  children,
-  tenantRPC,
-  value = {},
-}: {
-  children: ReactNode;
-  tenantRPC?: string;
-  value?: Partial<NodeInfoProviderInterface>;
-}) => {
-  const [state, setState] = useState(value);
+const NodeInfoProvider = ({ children, tenantRPC }: { children: ReactNode; tenantRPC?: string }) => {
+  const [state, setState] = useState({} as Partial<NodeInfoProviderInterface>);
   const [currentTenantRPC, setCurrentTenantRPC] = useState<string | undefined>(undefined);
   const [pendingInitiationPromise, setPendingInitiationPromise] = useState<Promise<unknown> | undefined>(undefined);
+
+  async function updateStateWithChainProperties(api: ApiPromise) {
+    const bestNumberFinalize = await api.derive.chain.bestNumber();
+    const chainProperties = await api.registry.getChainProperties();
+    const ss58Format = chainProperties?.get('ss58Format').toString();
+    const tokenDecimals = Number(
+      chainProperties
+        ?.get('tokenDecimals')
+        .toString()
+        .replace(/[\\[\]]/g, ''),
+    );
+    const tokenSymbol = chainProperties
+      ?.get('tokenSymbol')
+      .toString()
+      .replace(/[\\[\]]/g, '');
+
+    setState((prevState) => ({
+      ...prevState,
+      bestNumberFinalize: Number(bestNumberFinalize),
+      ss58Format: Number(ss58Format),
+      tokenDecimals,
+      tokenSymbol,
+      // TODO: same as for the api we could create a common interface for fetching data from indexer (swap assets, pools, other info)
+      // and pass the instance based on tenant to this context to be used in Swap, Pools components...
+      api,
+    }));
+  }
+
+  async function updateStateWithSystemInfo(api: ApiPromise) {
+    const [chain, nodeName, nodeVersion] = await Promise.all([
+      api.rpc.system.chain(),
+      api.rpc.system.name(),
+      api.rpc.system.version(),
+    ]);
+
+    setState((prevState) => ({
+      ...prevState,
+      chain: chain.toString(),
+      nodeName: nodeName.toString(),
+      nodeVersion: nodeVersion.toString(),
+    }));
+  }
+
+  async function handleConnectionError(error: Error) {
+    console.error('Error while connecting to the node:', error);
+    toast('Error while connecting to the node. Refresh the page to re-connect.', { type: toast.TYPE.ERROR });
+  }
 
   useEffect(() => {
     let disconnect: () => void = () => undefined;
@@ -43,72 +93,23 @@ const NodeInfoProvider = ({
     }
 
     const connect = async () => {
-      console.log('connecting to', tenantRPC);
-
       const provider = new WsProvider(tenantRPC, false);
       await provider.connect();
-      const api = await ApiPromise.create(
-        options({
-          provider,
-          rpc,
-          // These are necessary so that the promise throws the error
-          throwOnConnect: true,
-          throwOnUnknown: true,
-        }),
-      );
 
-      const bestNumberFinalize = await api.derive.chain.bestNumber();
-      const chainProperties = await api.registry.getChainProperties();
-      const ss58Format = chainProperties?.get('ss58Format').toString();
-      const tokenDecimals = Number(
-        chainProperties
-          ?.get('tokenDecimals')
-          .toString()
-          .replace(/[\\[\]]/g, ''),
-      );
-      const tokenSymbol = chainProperties
-        ?.get('tokenSymbol')
-        .toString()
-        .replace(/[\\[\]]/g, '');
-
-      setState((prevState) => ({
-        ...prevState,
-        bestNumberFinalize: Number(bestNumberFinalize),
-        ss58Format: Number(ss58Format),
-        tokenDecimals,
-        tokenSymbol,
-        // TODO: same as for the api we could create a common interface for fetching data from indexer (swap assets, pools, other info)
-        // and pass the instance based on tenant to this context to be used in Swap, Pools components...
-        api,
-      }));
-
-      const [chain, nodeName, nodeVersion] = await Promise.all([
-        api.rpc.system.chain(),
-        api.rpc.system.name(),
-        api.rpc.system.version(),
-      ]);
-
-      setState((prevState) => ({
-        ...prevState,
-        chain: chain.toString(),
-        nodeName: nodeName.toString(),
-        nodeVersion: nodeVersion.toString(),
-      }));
+      const api = await createApiPromise(provider);
+      await updateStateWithChainProperties(api);
+      await updateStateWithSystemInfo(api);
 
       disconnect = () => {
         api.disconnect();
       };
     };
 
-    console.log('pendingInitiationPromise', pendingInitiationPromise);
     if (!pendingInitiationPromise) {
       // We need this promise based approach to prevent race conditions when the user switches between tenants very quickly.
       // Otherwise, it might happen that the connection to the first endpoint takes longer and resolves later than
       // the connection to the second endpoint which would make us end up with a connection to the outdated endpoint.
-      const promise = connect().catch((error) => {
-        console.error('Error while connecting to the node:', error);
-        toast('Error while connecting to the node. Refresh the page to re-connect.', { type: toast.TYPE.ERROR });
-      });
+      const promise = connect().catch(handleConnectionError);
       setPendingInitiationPromise(promise);
     } else {
       pendingInitiationPromise.then(() => {
