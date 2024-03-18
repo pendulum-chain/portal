49c2987d6c1875deacc06755180c2e120a56e129
Kacper Szarkiewicz <43585069+Sharqiewicz@users.noreply.github.com>
2024-03-18 18:31:50 +0100

    enable Spacewalk for Pendulum (#372)
    Co-authored-by: Marcel Ebert <mail@marcel-ebert.de>

--- src/app.tsx
+++ src/app.tsx
@@ -8,8 +8,6 @@ import { NotFound } from './components/NotFound';
 import { SuspenseLoad } from './components/Suspense';
 import { config } from './config';
 import TermsAndConditions from './TermsAndConditions';
-import { useGlobalState } from './GlobalStateProvider';
-import { TenantName } from './models/Tenant';
 
 /**
  * Components need to be default exports inside the file for suspense loading to work properly
@@ -29,7 +27,6 @@ const Bridge = <SuspenseLoad importFn={() => import('./pages/bridge')} fallback=
 const Staking = <SuspenseLoad importFn={() => import('./pages/collators/Collators')} fallback={defaultPageLoader} />;
 
 export function App() {
-  const { tenantName } = useGlobalState();
   return (
     <>
       <Routes>
@@ -40,14 +37,8 @@ export function App() {
           <Route path="amm" element={Amm} />
           <Route path="stats" element={StatsPage} />
           <Route path="spacewalk">
-            {tenantName !== TenantName.Pendulum ? (
-              <>
-                <Route path="bridge" element={Bridge} />
-                <Route path="transfers" element={TransfersPage} />
-              </>
-            ) : (
-              <> </>
-            )}
+            <Route path="bridge" element={Bridge} />
+            <Route path="transfers" element={TransfersPage} />
           </Route>
           <Route path="nabla" Component={() => <AppsProvider app="nabla" />}>
             <Route path="" element={NablaPage} />
