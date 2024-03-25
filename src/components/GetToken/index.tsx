0a483e65ad0154df273c13bf87fd05666f38dd47
Kacper Szarkiewicz <szarkiewiczmail@gmail.com>
2024-03-25 16:14:52 +0100

    show GET AMPE button

--- src/components/GetToken/index.tsx
+++ src/components/GetToken/index.tsx
@@ -1,10 +1,12 @@
 import { Button } from 'react-daisyui';
+import { NavLink } from 'react-router-dom';
+
 import ampe from '../../assets/ampe.svg';
 import greenArrow from '../../assets/green-arrow.svg';
 import greenWarning from '../../assets/green-warning.svg';
 
 const InsufficientFundsTooltip = () => (
-  <div className="bg-black rounded-lg py-2 px-3 flex items-center">
+  <div className="bg-black rounded-lg h-8 px-3 flex items-center">
     <img src={greenWarning} className="h-4 w-4 mr-2" />
     <p className="text-[9px] text-primary">
       Insufficient funds to <br /> execute transactions
@@ -19,14 +21,19 @@ const JumpingArrow = () => (
 );
 
 export const GetToken = () => {
+  const link = '/foucoco/gas';
+
   return (
     <section className="flex items-center">
       <InsufficientFundsTooltip />
       <JumpingArrow />
-      <Button size="sm" color="primary" className="text-sm px-2 sm:px-3" type="button">
-        <img src={ampe} className="mr-1.5" />
-        <p className="text-black">GET AMPE</p>
-      </Button>
+
+      <NavLink to={link}>
+        <Button size="sm" color="primary" className="text-sm px-2 sm:px-3" type="button">
+          <img src={ampe} className="mr-1.5" />
+          <p className="text-black">GET AMPE</p>
+        </Button>
+      </NavLink>
     </section>
   );
 };
