74ebdf24eed55db8d8032c7d028ed99f3d416357
Kacper Szarkiewicz <szarkiewiczmail@gmail.com>
2024-04-03 12:53:00 +0200

    change From focus text color

--- src/components/Form/From/InputField/index.tsx
+++ src/components/Form/From/InputField/index.tsx
@@ -10,7 +10,7 @@ export const InputField = ({ register, readOnly = false }: InputFieldProps) => (
   <div className="w-full flex justify-between">
     <div className="flex-grow text-4xl text-black font-outfit">
       <Input
-        className="input-ghost w-full text-4xl font-outfit pl-0 focus:outline-none text-white"
+        className="input-ghost w-full text-4xl font-outfit pl-0 focus:outline-none focus:text-white text-white"
         type="number"
         step="any"
         readOnly={readOnly}
