47f9ef6e02bf39b8b496890a93c9a34a7fcb6b83
Kacper Szarkiewicz <szarkiewiczmail@gmail.com>
2024-04-10 12:22:39 +0200

    fix description text

--- src/components/Form/From/Description/index.tsx
+++ src/components/Form/From/Description/index.tsx
@@ -6,5 +6,5 @@ interface FromDescriptionProps {
 export const FromDescription = ({ customText, network }: FromDescriptionProps) => {
   if (!customText && !network) return <></>;
 
-  return <div className="text-sm mt-px text-secondary-content">{customText || network || ''}</div>;
+  return <div className="text-sm mt-px text-secondary-content">{customText || `From ${network}`}</div>;
 };
