43e4c3381f92a66937b8299425311de801a4eae9
Kacper Szarkiewicz <szarkiewiczmail@gmail.com>
2024-04-15 15:54:28 +0200

    update text confirmation dialog

--- src/pages/bridge/Redeem/ConfirmationDialog.tsx
+++ src/pages/bridge/Redeem/ConfirmationDialog.tsx
@@ -38,8 +38,7 @@ export function ConfirmationDialog(props: ConfirmationDialogProps): JSX.Element 
         </div>
         <div className="mt-6">
           <div className="text-sm mt-2">
-            We will update the transaction when PEN payment is executed. This typically takes only a few minutes but may
-            sometimes take up to 6 hours.
+            We will update the transaction when PEN payment is executed. This typically takes only a few minutes.
           </div>
         </div>
       </>
