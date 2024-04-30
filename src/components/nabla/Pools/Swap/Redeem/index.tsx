a14b933f852bd4cd58e8e8ad43cf9c38ecc586d6
Torsten Stüber <15174476+TorstenStueber@users.noreply.github.com>
2024-04-30 06:34:03 -0300

    Fix maxmium amount on swap pool withdrawal and redeem

--- src/components/nabla/Pools/Swap/Redeem/index.tsx
+++ src/components/nabla/Pools/Swap/Redeem/index.tsx
@@ -1,5 +1,5 @@
 import { ArrowLeftIcon } from '@heroicons/react/24/outline';
-import { ChangeEvent } from 'preact/compat';
+import { ChangeEvent, useEffect } from 'preact/compat';
 import { Button, Range } from 'react-daisyui';
 import { PoolProgress } from '../..';
 import { config } from '../../../../../config';
@@ -12,26 +12,33 @@ import { SwapPoolColumn } from '../columns';
 import { useRedeem } from './useRedeem';
 import { TransactionProgress } from '../../../common/TransactionProgress';
 import { NablaTokenPrice } from '../../../common/NablaTokenPrice';
+import { FormProvider } from 'react-hook-form';
+import { NumberInput } from '../../../common/NumberInput';
 
 export interface RedeemProps {
   data: SwapPoolColumn;
 }
 
 const Redeem = ({ data }: RedeemProps): JSX.Element | null => {
+  const { toggle, mutation, onSubmit, balanceQuery, depositQuery, lpTokensDecimalAmount, updateStorage, form } =
+    useRedeem(data);
+
   const {
-    toggle,
-    mutation,
-    onSubmit,
-    balanceQuery,
-    depositQuery,
-    lpTokensDecimalAmount,
-    updateStorage,
-    form: {
-      register,
-      setValue,
-      formState: { errors },
-    },
-  } = useRedeem(data);
+    setError,
+    clearErrors,
+    register,
+    setValue,
+    formState: { errors },
+  } = form;
+
+  useEffect(() => {
+    if (depositQuery.balance !== undefined && lpTokensDecimalAmount > depositQuery.balance) {
+      setError('amount', { type: 'custom', message: 'Amount exceeds owned LP tokens' });
+    } else {
+      clearErrors('amount');
+    }
+  }, [lpTokensDecimalAmount, depositQuery.balance, setError, clearErrors]);
+
   const deposit = depositQuery.balance || 0;
 
   const hideCss = !mutation.isIdle ? 'hidden' : '';
@@ -47,105 +54,106 @@ const Redeem = ({ data }: RedeemProps): JSX.Element | null => {
           </Button>
           <h3 className="text-3xl font-normal">Redeem {data.token?.symbol}</h3>
         </div>
-        <form onSubmit={onSubmit}>
-          <div className="flex justify-between align-end text-sm text-initial my-3">
-            <p>Deposited: {depositQuery.isLoading ? numberLoader : `${depositQuery.formatted || 0} LP`}</p>
-            <p className="text-neutral-500 text-right">
-              Balance: {balanceQuery.isLoading ? numberLoader : `${balanceQuery.formatted || 0} ${data.token.symbol}`}
-            </p>
-          </div>
-          <div className="relative rounded-lg bg-neutral-100 dark:bg-neutral-700 p-4">
-            <div className="flex items-center justify-between mb-4">
-              <input
-                type="text"
-                autoFocus
-                className="input-ghost w-full text-4xl font-2"
-                placeholder="0.0"
-                max={deposit}
-                {...register('amount')}
-              />
-              <Button
-                className="bg-neutral-200 dark:bg-neutral-800 px-4 rounded-2xl"
+        <FormProvider {...form}>
+          <form onSubmit={onSubmit}>
+            <div className="flex justify-between align-end text-sm text-initial my-3">
+              <p>Deposited: {depositQuery.isLoading ? numberLoader : `${depositQuery.formatted || 0} LP`}</p>
+              <p className="text-neutral-500 text-right">
+                Balance: {balanceQuery.isLoading ? numberLoader : `${balanceQuery.formatted || 0} ${data.token.symbol}`}
+              </p>
+            </div>
+            <div className="relative rounded-lg bg-neutral-100 dark:bg-neutral-700 p-4">
+              <div className="flex items-center justify-between mb-4">
+                <NumberInput
+                  type="text"
+                  autoFocus
+                  className="input-ghost w-full text-4xl font-2"
+                  placeholder="0.0"
+                  registerName="amount"
+                />
+                <Button
+                  className="bg-neutral-200 dark:bg-neutral-800 px-4 rounded-2xl"
+                  size="sm"
+                  type="button"
+                  onClick={() =>
+                    setValue('amount', deposit, {
+                      shouldDirty: true,
+                      shouldTouch: true,
+                    })
+                  }
+                >
+                  MAX
+                </Button>
+                <TransactionSettingsDropdown
+                  setSlippage={(slippage) => {
+                    setValue('slippage', slippage);
+                    updateStorage({ slippage });
+                  }}
+                  slippageProps={register('slippage', {
+                    valueAsNumber: true,
+                    onChange: (ev) =>
+                      updateStorage({
+                        slippage: Number(ev.currentTarget.value),
+                      }),
+                  })}
+                />
+              </div>
+              <Range
+                color="primary"
+                min={0}
+                max={100}
                 size="sm"
-                type="button"
-                onClick={() =>
-                  setValue('amount', deposit, {
+                value={lpTokensDecimalAmount ? (lpTokensDecimalAmount / deposit) * 100 : 0}
+                onChange={(ev: ChangeEvent<HTMLInputElement>) =>
+                  setValue('amount', roundNumber((Number(ev.currentTarget.value) / 100) * deposit, 4), {
                     shouldDirty: true,
-                    shouldTouch: true,
+                    shouldTouch: false,
                   })
                 }
-              >
-                MAX
-              </Button>
-              <TransactionSettingsDropdown
-                setSlippage={(slippage) => {
-                  setValue('slippage', slippage);
-                  updateStorage({ slippage });
-                }}
-                slippageProps={register('slippage', {
-                  valueAsNumber: true,
-                  onChange: (ev) =>
-                    updateStorage({
-                      slippage: Number(ev.currentTarget.value),
-                    }),
-                })}
               />
             </div>
-            <Range
-              color="primary"
-              min={0}
-              max={100}
-              size="sm"
-              value={lpTokensDecimalAmount ? (lpTokensDecimalAmount / deposit) * 100 : 0}
-              onChange={(ev: ChangeEvent<HTMLInputElement>) =>
-                setValue('amount', roundNumber((Number(ev.currentTarget.value) / 100) * deposit, 4), {
-                  shouldDirty: true,
-                  shouldTouch: false,
-                })
-              }
-            />
-          </div>
-          <div className="relative flex w-full flex-col gap-4 rounded-lg bg-neutral-100 dark:bg-neutral-700 text-neutral-500 dark:text-neutral-300 p-4 mt-4">
-            <div className="flex items-center justify-between">
-              <div>Security fee</div>
-              <div>{config.backstop.securityFee * 100}%</div>
-            </div>
-            <div className="flex items-center justify-between">
-              <div>Price</div>
-              <div>
-                <NablaTokenPrice address={data.token.id} prefix={`1 ${data.token.symbol} = `} fallback="-" />
+            <div className="relative flex w-full flex-col gap-4 rounded-lg bg-neutral-100 dark:bg-neutral-700 text-neutral-500 dark:text-neutral-300 p-4 mt-4">
+              <div className="flex items-center justify-between">
+                <div>Security fee</div>
+                <div>{config.backstop.securityFee * 100}%</div>
+              </div>
+              <div className="flex items-center justify-between">
+                <div>Price</div>
+                <div>
+                  <NablaTokenPrice address={data.token.id} prefix={`1 ${data.token.symbol} = `} fallback="-" />
+                </div>
+              </div>
+              <div className="flex items-center justify-between">
+                <div>Deposit</div>
+                <div>{roundNumber(deposit)}</div>
+              </div>
+              <div className="flex items-center justify-between">
+                <div>Pool share</div>
+                <div>
+                  {minMax(
+                    calcSharePercentage(rawToDecimal(data.totalSupply || 0, data.lpTokenDecimals).toNumber(), deposit),
+                  )}
+                  %
+                </div>
               </div>
             </div>
-            <div className="flex items-center justify-between">
-              <div>Deposit</div>
-              <div>{roundNumber(deposit)}</div>
+            <div className="mt-8">
+              <Validation className="text-center mb-2" errors={errors} />
+              <Button color="primary" className="w-full" type="submit" disabled={Object.keys(errors).length > 0}>
+                Redeem
+              </Button>
+              <Button
+                color="secondary"
+                className="mt-2 w-full"
+                type="button"
+                disable={mutation.isLoading}
+                onClick={() => toggle()}
+              >
+                Cancel
+              </Button>
             </div>
-            <div className="flex items-center justify-between">
-              <div>Pool share</div>
-              <div>
-                {minMax(
-                  calcSharePercentage(rawToDecimal(data.totalSupply || 0, data.lpTokenDecimals).toNumber(), deposit),
-                )}
-                %
-              </div>
-            </div>
-          </div>
-          <div className="mt-8">
-            <Validation className="text-center mb-2" errors={errors} />
-            <Button color="primary" className="w-full" type="submit">
-              Redeem
-            </Button>
-            <Button
-              color="secondary"
-              className="mt-2 w-full"
-              type="button"
-              disable={mutation.isLoading}
-              onClick={() => toggle()}
-            >
-              Cancel
-            </Button>
-          </div>
-        </form>
+          </form>
+        </FormProvider>
       </div>
     </div>
   );
--- src/components/nabla/Pools/Swap/WithdrawLiquidity/index.tsx
+++ src/components/nabla/Pools/Swap/WithdrawLiquidity/index.tsx
@@ -1,5 +1,5 @@
 import { ArrowLeftIcon } from '@heroicons/react/24/outline';
-import { ChangeEvent } from 'preact/compat';
+import { ChangeEvent, useEffect } from 'preact/compat';
 import { Button, Range } from 'react-daisyui';
 import { PoolProgress } from '../..';
 import { swapPoolAbi } from '../../../../../contracts/nabla/SwapPool';
@@ -12,25 +12,37 @@ import { ModalTypes } from '../Modals/types';
 import { useSwapPoolWithdrawLiquidity } from './useWithdrawLiquidity';
 import { TransactionProgress } from '../../../common/TransactionProgress';
 import { TokenAmount } from '../../../common/TokenAmount';
+import { FormProvider } from 'react-hook-form';
+import { NumberInput } from '../../../common/NumberInput';
 
 export interface WithdrawLiquidityProps {
   data: SwapPoolColumn;
 }
 
 const WithdrawLiquidity = ({ data }: WithdrawLiquidityProps): JSX.Element | null => {
+  const { toggle, mutation, onSubmit, balanceQuery, depositQuery, amount, form } = useSwapPoolWithdrawLiquidity(
+    data.id,
+    data.token.id,
+    data.token.decimals,
+    data.lpTokenDecimals,
+  );
+
   const {
-    toggle,
-    mutation,
-    onSubmit,
-    balanceQuery,
-    depositQuery,
-    amount,
-    form: {
-      register,
-      setValue,
-      formState: { errors },
-    },
-  } = useSwapPoolWithdrawLiquidity(data.id, data.token.id, data.token.decimals, data.lpTokenDecimals);
+    setError,
+    clearErrors,
+    register,
+    setValue,
+    formState: { errors },
+  } = form;
+
+  useEffect(() => {
+    if (depositQuery.balance !== undefined && amount > depositQuery.balance) {
+      setError('amount', { type: 'custom', message: 'Amount exceeds owned LP tokens' });
+    } else {
+      clearErrors('amount');
+    }
+  }, [amount, depositQuery.balance, setError, clearErrors]);
+
   const depositedLpTokensDecimalAmount = depositQuery.balance || 0;
 
   const hideCss = !mutation.isIdle ? 'hidden' : '';
@@ -46,130 +58,131 @@ const WithdrawLiquidity = ({ data }: WithdrawLiquidityProps): JSX.Element | null
           </Button>
           <h3 className="text-3xl font-normal">Withdraw {data.token?.symbol}</h3>
         </div>
-        <form onSubmit={onSubmit}>
-          <div className="flex justify-between align-end text-sm text-initial my-3">
-            <p>Deposited: {depositQuery.isLoading ? numberLoader : `${depositQuery.formatted || 0} LP`}</p>
-            <p className="text-neutral-500 text-right">
-              Balance: {balanceQuery.isLoading ? numberLoader : `${balanceQuery.formatted || 0} ${data.token.symbol}`}
-            </p>
-          </div>
-          <div className="relative rounded-lg bg-neutral-100 dark:bg-neutral-700 p-4">
-            <div className="flex items-center gap-1 justify-between mb-4">
-              <input
-                type="text"
-                autoFocus
-                className="input-ghost flex-grow w-full text-4xl font-2"
-                placeholder="0.0"
-                max={depositedLpTokensDecimalAmount}
-                {...register('amount')}
-              />
-              <Button
-                className="bg-neutral-200 dark:bg-neutral-800 px-3 rounded-2xl"
-                size="sm"
-                type="button"
-                onClick={() =>
-                  setValue('amount', depositedLpTokensDecimalAmount / 2, {
-                    shouldDirty: true,
-                    shouldTouch: true,
-                  })
-                }
-              >
-                50%
-              </Button>
-              <Button
-                className="bg-neutral-200 dark:bg-neutral-800 px-3 rounded-2xl"
-                size="sm"
-                type="button"
-                onClick={() =>
-                  setValue('amount', depositedLpTokensDecimalAmount, {
-                    shouldDirty: true,
-                    shouldTouch: true,
-                  })
-                }
-              >
-                MAX
-              </Button>
+        <FormProvider {...form}>
+          <form onSubmit={onSubmit}>
+            <div className="flex justify-between align-end text-sm text-initial my-3">
+              <p>Deposited: {depositQuery.isLoading ? numberLoader : `${depositQuery.formatted || 0} LP`}</p>
+              <p className="text-neutral-500 text-right">
+                Balance: {balanceQuery.isLoading ? numberLoader : `${balanceQuery.formatted || 0} ${data.token.symbol}`}
+              </p>
             </div>
-            <Range
-              color="primary"
-              min={0}
-              max={100}
-              size="sm"
-              value={amount ? (amount / depositedLpTokensDecimalAmount) * 100 : 0}
-              onChange={(ev: ChangeEvent<HTMLInputElement>) =>
-                setValue(
-                  'amount',
-                  roundNumber((Number(ev.currentTarget.value) / 100) * depositedLpTokensDecimalAmount, 4),
-                  {
-                    shouldDirty: true,
-                    shouldTouch: false,
-                  },
-                )
-              }
-            />
-          </div>
-          <div className="relative flex w-full flex-col gap-4 rounded-lg bg-neutral-100 dark:bg-neutral-700 text-neutral-500 dark:text-neutral-300 p-4 mt-4">
-            <div className="flex items-center justify-between">
-              <div>Amount</div>
-              <div>
-                <TokenAmount
-                  address={data.id}
-                  abi={swapPoolAbi}
-                  lpTokenDecimalAmount={amount}
-                  symbol={` ${data.token.symbol}`}
-                  fallback={0}
-                  poolTokenDecimals={data.token.decimals}
-                  lpTokenDecimals={data.lpTokenDecimals}
+            <div className="relative rounded-lg bg-neutral-100 dark:bg-neutral-700 p-4">
+              <div className="flex items-center gap-1 justify-between mb-4">
+                <NumberInput
+                  type="text"
+                  autoFocus
+                  className="input-ghost flex-grow w-full text-4xl font-2"
+                  placeholder="0.0"
+                  registerName="amount"
                 />
+                <Button
+                  className="bg-neutral-200 dark:bg-neutral-800 px-3 rounded-2xl"
+                  size="sm"
+                  type="button"
+                  onClick={() =>
+                    setValue('amount', depositedLpTokensDecimalAmount / 2, {
+                      shouldDirty: true,
+                      shouldTouch: true,
+                    })
+                  }
+                >
+                  50%
+                </Button>
+                <Button
+                  className="bg-neutral-200 dark:bg-neutral-800 px-3 rounded-2xl"
+                  size="sm"
+                  type="button"
+                  onClick={() =>
+                    setValue('amount', depositedLpTokensDecimalAmount, {
+                      shouldDirty: true,
+                      shouldTouch: true,
+                    })
+                  }
+                >
+                  MAX
+                </Button>
               </div>
-            </div>
-            <div className="flex items-center justify-between">
-              <div>Deposit</div>
-              <div>{roundNumber(depositedLpTokensDecimalAmount)}</div>
-            </div>
-            <div className="flex items-center justify-between">
-              <div>Pool share</div>
-              <div>
-                {minMax(
-                  calcSharePercentage(
-                    rawToDecimal(data.totalSupply || 0, data.lpTokenDecimals).toNumber(),
-                    depositedLpTokensDecimalAmount,
-                  ),
-                )}
-                %
-              </div>
-            </div>
-          </div>
-          <div className="mt-8">
-            <div className="text-center">
-              <button
-                type="button"
-                onClick={() =>
-                  toggle({
-                    type: ModalTypes.Redeem,
-                    props: { data },
-                  })
+              <Range
+                color="primary"
+                min={0}
+                max={100}
+                size="sm"
+                value={amount ? (amount / depositedLpTokensDecimalAmount) * 100 : 0}
+                onChange={(ev: ChangeEvent<HTMLInputElement>) =>
+                  setValue(
+                    'amount',
+                    roundNumber((Number(ev.currentTarget.value) / 100) * depositedLpTokensDecimalAmount, 4),
+                    {
+                      shouldDirty: true,
+                      shouldTouch: false,
+                    },
+                  )
                 }
-                className="btn btn-link mb-2 text-center"
-              >
-                Redeem from backstop pool
-              </button>
+              />
             </div>
-            <Validation className="text-center mb-2" errors={errors} />
-            <Button color="primary" className="w-full" type="submit">
-              Withdraw
-            </Button>
-            <Button
-              color="secondary"
-              className="mt-2 w-full"
-              type="button"
-              disable={mutation.isLoading}
-              onClick={() => toggle()}
-            >
-              Cancel
-            </Button>
-          </div>
-        </form>
+            <div className="relative flex w-full flex-col gap-4 rounded-lg bg-neutral-100 dark:bg-neutral-700 text-neutral-500 dark:text-neutral-300 p-4 mt-4">
+              <div className="flex items-center justify-between">
+                <div>Amount</div>
+                <div>
+                  <TokenAmount
+                    address={data.id}
+                    abi={swapPoolAbi}
+                    lpTokenDecimalAmount={amount}
+                    symbol={` ${data.token.symbol}`}
+                    fallback={0}
+                    poolTokenDecimals={data.token.decimals}
+                    lpTokenDecimals={data.lpTokenDecimals}
+                  />
+                </div>
+              </div>
+              <div className="flex items-center justify-between">
+                <div>Deposit</div>
+                <div>{roundNumber(depositedLpTokensDecimalAmount)}</div>
+              </div>
+              <div className="flex items-center justify-between">
+                <div>Pool share</div>
+                <div>
+                  {minMax(
+                    calcSharePercentage(
+                      rawToDecimal(data.totalSupply || 0, data.lpTokenDecimals).toNumber(),
+                      depositedLpTokensDecimalAmount,
+                    ),
+                  )}
+                  %
+                </div>
+              </div>
+            </div>
+            <div className="mt-8">
+              <Validation className="text-center mb-2" errors={errors} />
+              <div className="text-center">
+                <button
+                  type="button"
+                  onClick={() =>
+                    toggle({
+                      type: ModalTypes.Redeem,
+                      props: { data },
+                    })
+                  }
+                  className="btn btn-link mb-2 text-center"
+                >
+                  Redeem from backstop pool
+                </button>
+              </div>
+              <Button color="primary" className="w-full" type="submit" disabled={Object.keys(errors).length > 0}>
+                Withdraw
+              </Button>
+              <Button
+                color="secondary"
+                className="mt-2 w-full"
+                type="button"
+                disable={mutation.isLoading}
+                onClick={() => toggle()}
+              >
+                Cancel
+              </Button>
+            </div>
+          </form>
+        </FormProvider>
       </div>
     </div>
   );
