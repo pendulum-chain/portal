522d2f65e5bdf67e42f8e25996de6c150afbea6c
Kacper Szarkiewicz <43585069+Sharqiewicz@users.noreply.github.com>
2024-04-30 21:16:12 +0200

    chore: Use reusable Dialog component for TransferDialog
    * use reusable Dialog component for TransferDialog
    
    * define --modal-border tailwindcss var and improve modal visibility with border

--- src/pages/collators/dialogs/Dialog.tsx
+++ src/pages/collators/dialogs/Dialog.tsx
@@ -7,13 +7,14 @@ interface DialogProps {
   headerText?: string;
   content: JSX.Element;
   actions: JSX.Element;
+  id?: string;
 }
 
 export const Dialog: React.FC<DialogProps> = (props) => {
-  const { visible, onClose, headerText, content, actions } = props;
+  const { visible, onClose, headerText, content, actions, id } = props;
 
   return (
-    <Modal className="bg-base-200" open={visible}>
+    <Modal className="bg-base-200 border border-[--modal-border]" open={visible} id={id}>
       <Modal.Header className={`text-2xl claim-title flex mb-5 ${headerText ? 'justify-between' : 'justify-end'}`}>
         {headerText} <CloseButton onClick={onClose} />
       </Modal.Header>
