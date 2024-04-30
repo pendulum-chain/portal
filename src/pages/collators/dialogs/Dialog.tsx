1480fa1a73c6b60414afc70d39025c277ecfd247
Kacper Szarkiewicz <szarkiewiczmail@gmail.com>
2024-04-30 21:12:03 +0200

    define --modal-border tailwindcss var and improve modal visibility with border

--- src/pages/collators/dialogs/Dialog.tsx
+++ src/pages/collators/dialogs/Dialog.tsx
@@ -14,7 +14,7 @@ export const Dialog: React.FC<DialogProps> = (props) => {
   const { visible, onClose, headerText, content, actions, id } = props;
 
   return (
-    <Modal className="bg-base-200" open={visible} id={id}>
+    <Modal className="bg-base-200 border border-[--modal-border]" open={visible} id={id}>
       <Modal.Header className={`text-2xl claim-title flex mb-5 ${headerText ? 'justify-between' : 'justify-end'}`}>
         {headerText} <CloseButton onClick={onClose} />
       </Modal.Header>
