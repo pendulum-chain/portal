8ceefc7988b840cf848c4f0ea2bb6fcba82d6022
Kacper Szarkiewicz <szarkiewiczmail@gmail.com>
2024-05-17 20:25:02 +0200

    adjust Tab to newest DaisyUI

--- src/pages/bridge/index.tsx
+++ src/pages/bridge/index.tsx
@@ -44,21 +44,26 @@ function Bridge(): JSX.Element | null {
     }
   }, [chain, nativeCurrency, tabValue, wrappedCurrencySuffix]);
 
+  const getTabProps = (index: number) => ({
+    active: tabValue === index,
+    onClick: () => setTabValue(index),
+  });
+
   return chain ? (
     <BridgeContext.Provider value={{ selectedAsset, setSelectedAsset }}>
       <div className="h-full flex items-center justify-center mt-4">
         <SettingsDialog visible={settingsVisible} onClose={() => setSettingsVisible(false)} />
         <Card className="bridge-card bg-base-200 min-h-500 w-full max-w-[520px] rounded-lg">
           <div className="flex justify-between px-5 mt-5">
-            <Tabs className="flex w-5/6 flex-grow justify-center" boxed value={tabValue} onChange={setTabValue}>
-              <Tabs.Tab className="w-2/5 h-fit p-2" value={0}>
+            <Tabs className="flex w-5/6 flex-grow justify-center tabs-boxed">
+              <Tabs.Tab className="w-2/5 h-fit p-2" {...getTabProps(0)}>
                 {chain.toLowerCase() === TenantName.Pendulum && <PendulumLogo className="w-6 h-6 mr-1" />}
                 {(chain.toLowerCase() === TenantName.Amplitude || chain.toLowerCase() === TenantName.Foucoco) && (
                   <AmplitudeLogo className="w-6 h-6 mr-1" />
                 )}
                 To {chain}
               </Tabs.Tab>
-              <Tabs.Tab className="w-2/5 h-fit p-2" value={1}>
+              <Tabs.Tab className="w-2/5 h-fit p-2" {...getTabProps(1)}>
                 <StellarLogo className="w-6 h-6 mr-1" />
                 To Stellar
               </Tabs.Tab>
