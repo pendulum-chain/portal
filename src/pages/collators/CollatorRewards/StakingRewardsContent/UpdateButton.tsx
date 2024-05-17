bc4a9460cd8e3ef4200172e395400a58e528e4fa
Kacper Szarkiewicz <szarkiewiczmail@gmail.com>
2024-05-17 20:28:15 +0200

    fix update button responsiveness

--- src/pages/collators/CollatorRewards/StakingRewardsContent/UpdateButton.tsx
+++ src/pages/collators/CollatorRewards/StakingRewardsContent/UpdateButton.tsx
@@ -10,7 +10,7 @@ export const UpdateButton: React.FC<UpdateButtonProps> = ({ loading, disabled, o
   <Button
     loading={loading}
     onClick={onClick}
-    className="btn btn-primary w-full xs:w-1/2 xs:mr-2 mb-2 rounded-md p-0 leading-3 min-h-fit max-h-10 min-w-20"
+    className="btn btn-primary w-full xs:w-1/2 xs:mr-2 mb-2 rounded-md p-0 leading-3 min-h-fit max-h-10"
     disabled={disabled}
   >
     {loading ? '' : 'Update'}
