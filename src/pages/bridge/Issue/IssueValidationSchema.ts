176ac386fc617c550a58fd35995ecf677882b4d5
Kacper Szarkiewicz <43585069+Sharqiewicz@users.noreply.github.com>
2024-05-22 13:28:20 +0200

    Update error message for insufficient security deposit
    * change spacewalk issue error, and tokenSymbol
    
    * make handling errors in spacewalk > bridge open/closed principle and fix 'Bridge' button disabled
    
    * improve code readability
    
    * check errors on every change in spacewalk bridge

--- src/pages/bridge/Issue/IssueValidationSchema.ts
+++ src/pages/bridge/Issue/IssueValidationSchema.ts
@@ -2,7 +2,7 @@ import * as Yup from 'yup';
 import { IssueFormValues } from '.';
 import { transformNumber } from '../../../helpers/yup';
 
-export function getIssueValidationSchema(maxIssuable: number, balance: number) {
+export function getIssueValidationSchema(maxIssuable: number, balance: number, tokenSymbol?: string) {
   return Yup.object<IssueFormValues>().shape({
     amount: Yup.number()
       .typeError('Value is invalid.')
@@ -12,6 +12,6 @@ export function getIssueValidationSchema(maxIssuable: number, balance: number) {
       .max(maxIssuable, 'The vault cannot issue that amount of the selected asset.'),
     securityDeposit: Yup.number()
       .transform(transformNumber)
-      .max(balance, "You don't have enough balance to pay the deposit fees."),
+      .max(balance, `Insufficient ${tokenSymbol} to cover the security deposit.`),
   });
 }
