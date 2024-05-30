06ac4c8ff0983848b9c49ed534687d2e36bbbe3b
Kacper Szarkiewicz <szarkiewiczmail@gmail.com>
2024-05-30 14:45:57 +0200

    implement swap assets button

---
+++ src/components/SwapAssetsButton/index.tsx
@@ -0,0 +1,25 @@
+import { useState } from 'preact/hooks';
+import { SwapAssetsButtonIcon } from '../../assets/SwapAssetsButtonIcon';
+
+interface SwapAssetsButtonProps {
+  onClick?: () => void;
+}
+
+export const SwapAssetsButton = ({ onClick }: SwapAssetsButtonProps) => {
+  const [isRotated, setIsRotated] = useState(false);
+  const toggleRotation = () => setIsRotated(!isRotated);
+
+  const onButtonClick = () => {
+    toggleRotation();
+    onClick && onClick();
+  };
+
+  return (
+    <button type="button" onClick={onButtonClick}>
+      <div className={`flex justify-center duration-300 cursor-pointer ${isRotated ? 'rotate-180' : ''}`}>
+        <SwapAssetsButtonIcon />
+      </div>
+      <span className="visually-hidden">Swap selected assets</span>
+    </button>
+  );
+};
