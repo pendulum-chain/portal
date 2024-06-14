b8387a814a23f4b7b8e62523aa1d48cd02439cad
Kacper Szarkiewicz <szarkiewiczmail@gmail.com>
2024-06-14 14:31:37 +0200

    restructure ConnectWallet file structure

--- src/components/Wallet/index.tsx
+++ src/components/Wallet/index.tsx
@@ -1,12 +1,12 @@
-import { Disconnect } from './Disconnect';
+import { DisconnectModal } from './modals/DisconnectModal';
 import { useGlobalState } from '../../GlobalStateProvider';
-import { Connect, ConnectProps } from './Connect';
+import { ConnectModal, ConnectProps } from './modals/ConnectModal';
 
 const OpenWallet = (props: ConnectProps): JSX.Element => {
   const { walletAccount } = useGlobalState();
   const { address } = walletAccount || {};
 
-  return address ? <Disconnect /> : <Connect {...props} />;
+  return address ? <DisconnectModal /> : <ConnectModal {...props} />;
 };
 
 export default OpenWallet;
