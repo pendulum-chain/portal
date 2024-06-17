bbe2d314db23b189a178b0501cabb4eedf1f1236
Kacper Szarkiewicz <szarkiewiczmail@gmail.com>
2024-06-17 08:38:23 +0200

    Add ConnectModalContentLoading

--- src/components/Wallet/modals/ConnectModal/ConnectModalContent.tsx
+++ src/components/Wallet/modals/ConnectModal/ConnectModalContent.tsx
@@ -1,56 +1,30 @@
-import { StateUpdater, useCallback, useEffect, useState } from 'preact/hooks';
-import { Wallet, WalletAccount, getWallets } from '@talismn/connect-wallets';
-import { Collapse } from 'react-daisyui';
+import { Collapse, Loading } from 'react-daisyui';
 import { ConnectModalWalletsList } from './ConnectModalList/ConnectModalWalletsList';
-import { useGlobalState } from '../../../../GlobalStateProvider';
 import { ConnectModalAccountsList } from './ConnectModalList/ConnectModalAccountsList';
+import { useConnectWallet } from '../../../../hooks/useConnectWallet';
+
+const ConnectModalLoading = () => (
+  <article className="flex flex-col items-center justify-center">
+    <Loading variant="dots" size="lg" />
+    <h1 className="text-2xl">Connecting wallet</h1>
+    <p className="mt-2.5 w-52 text-center text-sm">Please approve walletName and approve transaction.</p>
+  </article>
+);
 
 export const ConnectModalContent = () => {
-  const [wallets, setWallets] = useState<Wallet[]>();
-  const [selectedWallet, setSelectedWallet] = useState<Wallet>();
-  const [accounts, setAccounts] = useState<WalletAccount[]>([]);
+  const { wallets, accounts, selectWallet } = useConnectWallet();
 
-  const { dAppName, walletAccount } = useGlobalState();
-  const [unsubscribe, setUnsubscribe] = useState<StateUpdater<Record<string, () => unknown>>>();
-
-  useEffect(() => {
-    const wallets = getWallets();
-    const installedWallets = wallets.filter((wallet) => wallet.installed);
-    setWallets(installedWallets || wallets);
-
-    return () => {
-      setSelectedWallet(undefined);
-    };
-  }, []);
-
-  const onWalletListSelected = useCallback(
-    async (wallet: Wallet) => {
-      setSelectedWallet(wallet);
-
-      try {
-        await wallet.enable(dAppName);
-
-        await wallet.subscribeAccounts((walletAccounts) => {
-          if (walletAccounts) {
-            setAccounts(walletAccounts);
-          }
-        });
-      } catch (err) {
-        console.warn(err);
-      }
-    },
-    [dAppName],
-  );
+  // if (loading) return <ConnectModalLoading />;
 
   return (
     <article className="flex flex-wrap gap-2">
       <Collapse defaultChecked icon="arrow" open name="wallets">
         <Collapse.Title>Select Wallet</Collapse.Title>
         <Collapse.Content>
-          <ConnectModalWalletsList items={wallets} onClick={(wallet) => onWalletListSelected(wallet)} />
-          <p className="text-center text-xs mt-3.5">
+          <ConnectModalWalletsList items={wallets} onClick={(wallet) => selectWallet(wallet)} />
+          <p className="mt-3.5 text-center text-xs">
             Want to know more?
-            <a href="#" className="text-primary hover:underline ml-1">
+            <a href="#" className="ml-1 text-primary hover:underline">
               Learn more about wallets
             </a>
           </p>
@@ -59,7 +33,7 @@ export const ConnectModalContent = () => {
       <Collapse defaultChecked icon="arrow" open name="accounts">
         <Collapse.Title>Choose Account</Collapse.Title>
         <Collapse.Content>
-          <ConnectModalAccountsList accounts={accounts} />
+          <ConnectModalAccountsList accounts={accounts || []} />
         </Collapse.Content>
       </Collapse>
     </article>
