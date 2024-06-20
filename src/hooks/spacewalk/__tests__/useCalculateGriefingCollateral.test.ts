d25891e9762fd92564cdefa987052c15eb60468a
Marcel Ebert <mail@marcel-ebert.de>
2024-06-20 14:24:35 +0200

    Improve useCalculateGriefingCollateral.test.ts

--- src/hooks/spacewalk/__tests__/useCalculateGriefingCollateral.test.ts
+++ src/hooks/spacewalk/__tests__/useCalculateGriefingCollateral.test.ts
@@ -7,10 +7,12 @@ import { useCalculateGriefingCollateral } from '../useCalculateGriefingCollatera
 
 const STELLAR_ONE_WITH_DECIMALS = 1000000000000;
 const VALID_ASSET = new Asset('code', 'GASOCNHNNLYFNMDJYQ3XFMI7BYHIOCFW3GJEOWRPEGK2TDPGTG2E5EDW');
+// We assume that the price is the same for all assets
+const PRICE = 1;
 
 jest.mock('../../usePriceFetcher', () => ({
   usePriceFetcher: jest.fn().mockReturnValue({
-    getTokenPriceForCurrency: jest.fn().mockReturnValue(Promise.resolve(1)),
+    getTokenPriceForCurrency: jest.fn().mockReturnValue(Promise.resolve(PRICE)),
   }),
 }));
 
@@ -42,19 +44,25 @@ describe('useCalculateGriefingCollateral', () => {
     jest.useFakeTimers();
   });
 
-  it('should return 0 griefing collateral if amount is not valid', () => {
-    const { result } = renderHook(() => useCalculateGriefingCollateral(new Big(0), undefined));
+  it('should return 0 griefing collateral if amount is not valid', async () => {
+    const { result } = renderHook(() => useCalculateGriefingCollateral(new Big(0), VALID_ASSET));
 
-    expect(result.current.eq(new Big(0))).toBeTruthy();
+    const expectation = () => expect(result.current.eq(new Big(0))).toBeTruthy();
+
+    expectation();
+    await waitFor(expectation);
   });
 
-  it('should return 0 griefing collateral if bridgedAsset is not valid', () => {
+  it('should return 0 griefing collateral if bridgedAsset is not valid', async () => {
     const { result } = renderHook(() => useCalculateGriefingCollateral(new Big(1000), undefined));
 
-    expect(result.current.eq(new Big(0))).toBeTruthy();
+    const expectation = () => expect(result.current.eq(new Big(0))).toBeTruthy();
+
+    expectation();
+    await waitFor(expectation);
   });
 
-  it('should return 0 griefing collateral if griefingCollateralCurrency is not valid', () => {
+  it('should return 0 griefing collateral if griefingCollateralCurrency is not valid', async () => {
     mockGetFees.mockReturnValueOnce({
       griefingCollateralCurrency: undefined,
       issueGriefingCollateralFee: 0.05,
@@ -62,17 +70,26 @@ describe('useCalculateGriefingCollateral', () => {
 
     const { result } = renderHook(() => useCalculateGriefingCollateral(new Big(1000), VALID_ASSET));
 
-    expect(result.current.eq(new Big(0))).toBeTruthy();
+    const expectation = () => expect(result.current.eq(new Big(0))).toBeTruthy();
+
+    expectation();
+    await waitFor(expectation);
   });
 
   it('should calculate griefing collateral correctly', async () => {
-    const { result } = renderHook(() =>
-      useCalculateGriefingCollateral(new Big(STELLAR_ONE_WITH_DECIMALS), VALID_ASSET),
-    );
+    const amount = new Big(STELLAR_ONE_WITH_DECIMALS);
+    const { result } = renderHook(() => useCalculateGriefingCollateral(amount, VALID_ASSET));
 
-    // First retuned value (before useEffect is fired)
+    // First returned value (before useEffect is fired)
     expect(result.current.eq(0)).toBeTruthy();
 
-    await waitFor(() => expect(result.current.eq(0.05)).toBeTruthy());
+    const griefingFee = mockGetFees().issueGriefingCollateralFee;
+    // Since we assume the same price for all assets, the decimal griefing collateral is the amount * griefingFee / decimals
+    const griefingAmount = amount
+      .times(griefingFee)
+      .div(10 ** 12)
+      .toNumber();
+
+    await waitFor(() => expect(result.current.eq(griefingAmount)).toBeTruthy());
   });
 });
