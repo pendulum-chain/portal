cc7eac483e24ab9cd93a7ac32e0809c1c1ad527a
Kacper Szarkiewicz <szarkiewiczmail@gmail.com>
2024-06-25 22:17:47 +0200

    implement maxDecimals for StandardFrom AvaiableActions

--- src/components/Form/From/AvailableActions/index.tsx
+++ src/components/Form/From/AvailableActions/index.tsx
@@ -1,20 +1,39 @@
+import { trimMaxDecimals, USER_INPUT_MAX_DECIMALS } from '../../../../shared/parseNumbers/maxDecimals';
+
 interface AvailableActionsProps {
   max?: number;
+  maxDecimals?: number;
   setValue?: (n: number) => void;
 }
 
-export const AvailableActions = ({ max, setValue }: AvailableActionsProps) => (
-  <div className="flex gap-1 text-sm">
-    {max !== undefined && setValue !== undefined && (
-      <>
-        <span className="mr-1">Available: {max.toFixed(2)}</span>
-        <button className="text-primary hover:underline" onClick={() => setValue(Number(max) * 0.5)} type="button">
-          50%
-        </button>
-        <button className="text-primary hover:underline" onClick={() => setValue(Number(max))} type="button">
-          MAX
-        </button>
-      </>
-    )}
-  </div>
-);
+export const AvailableActions = ({
+  max,
+  maxDecimals = USER_INPUT_MAX_DECIMALS.PENDULUM,
+  setValue,
+}: AvailableActionsProps) => {
+  const handleSetValue = (percentage: number) => {
+    if (max !== undefined && setValue !== undefined) {
+      const trimmedValue = trimMaxDecimals(String(max * percentage), maxDecimals);
+      setValue(Number(trimmedValue));
+    }
+  };
+
+  const handleSetHalf = () => handleSetValue(0.5);
+  const handleSetMax = () => handleSetValue(1);
+
+  return (
+    <div className="flex gap-1 text-sm">
+      {max !== undefined && setValue !== undefined && (
+        <>
+          <span className="mr-1">Available: {max.toFixed(2)}</span>
+          <button className="text-primary hover:underline" onClick={handleSetHalf} type="button">
+            50%
+          </button>
+          <button className="text-primary hover:underline" onClick={handleSetMax} type="button">
+            MAX
+          </button>
+        </>
+      )}
+    </div>
+  );
+};
--- src/shared/parseNumbers/maxDecimals.ts
+++ src/shared/parseNumbers/maxDecimals.ts
@@ -1,38 +1,41 @@
 export enum USER_INPUT_MAX_DECIMALS {
-    PENDULUM = 12,
-    STELLAR = 7,
+  PENDULUM = 12,
+  STELLAR = 7,
 }
 
 export function exceedsMaxDecimals(value: unknown, maxDecimals: number) {
-    if (value === undefined || value === null) return true;
-    const decimalPlaces = value.toString().split('.')[1];
-    return decimalPlaces ? decimalPlaces.length > maxDecimals : false;
+  if (value === undefined || value === null) return true;
+  const decimalPlaces = value.toString().split('.')[1];
+  return decimalPlaces ? decimalPlaces.length > maxDecimals : false;
+}
+
+export function trimMaxDecimals(value: string, maxDecimals: number): string {
+  const [integer, decimal] = value.split('.');
+  return decimal ? `${integer}.${decimal.slice(0, maxDecimals)}` : value;
 }
 
 function isValidNumericInput(value: string): boolean {
-    return /^[0-9.,]*$/.test(value);
+  return /^[0-9.,]*$/.test(value);
 }
 
 function alreadyHasDecimal(e: KeyboardEvent) {
-    const decimalChars = ['.', ','];
+  const decimalChars = ['.', ','];
 
-    // In the onInput event, "," is replaced by ".", so we check if the e.target.value already contains a "."
-    return decimalChars.some((char) => e.key === char && e.target && (e.target as HTMLInputElement).value.includes('.'));
+  // In the onInput event, "," is replaced by ".", so we check if the e.target.value already contains a "."
+  return decimalChars.some((char) => e.key === char && e.target && (e.target as HTMLInputElement).value.includes('.'));
 }
 
-export function truncateIfExceedsMaxDecimals(value: string, maxDecimals: number): string {
-    if (exceedsMaxDecimals(value, maxDecimals)) {
-        return value.slice(0, -1);
-    }
-    return value;
+function truncateIfExceedsMaxDecimals(value: string, maxDecimals: number): string {
+  if (exceedsMaxDecimals(value, maxDecimals - 1)) {
+    return value.slice(0, -1);
+  }
+  return value;
 }
 
 export function handleOnKeyPressExceedsMaxDecimals(e: KeyboardEvent, maxDecimals: number): void {
-    if (!isValidNumericInput(e.key) || alreadyHasDecimal(e)) {
-        e.preventDefault();
-    }
-    const target = (e.target as HTMLInputElement);
-      if (exceedsMaxDecimals(target.value, maxDecimals - 1)) {
-        target.value = target.value.slice(0, -1);
-    }
+  if (!isValidNumericInput(e.key) || alreadyHasDecimal(e)) {
+    e.preventDefault();
+  }
+  const target = e.target as HTMLInputElement;
+  target.value = truncateIfExceedsMaxDecimals(target.value, maxDecimals);
 }
