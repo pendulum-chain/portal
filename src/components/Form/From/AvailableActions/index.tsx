7f43f1f916d96e6f7bc7cbc0c28e33f515bad003
Kacper Szarkiewicz <43585069+Sharqiewicz@users.noreply.github.com>
2024-06-27 18:00:33 +0200

    Remove 50% and Max from the Backstop pool withdraw UI (#496)
    * change color of nabla dialog inputs
    
    * fix nabla loading buttons styles
    
    * remove 50percent, max options from Amount Nabla input
    
    * refactor nabla modals to use reusable dialog component
    
    * add a space between nabla pool dialog asset and name
    
    * use AvailableActions component in Nabla Amount input
    
    * fix paths
    
    * use AvailableActions for Backstop Pool Deposit

--- src/components/Form/From/AvailableActions/index.tsx
+++ src/components/Form/From/AvailableActions/index.tsx
@@ -1,13 +1,14 @@
 interface AvailableActionsProps {
   max?: number;
   setValue?: (n: number) => void;
+  hideAvailableBalance?: boolean;
 }
 
-export const AvailableActions = ({ max, setValue }: AvailableActionsProps) => (
+export const AvailableActions = ({ max, setValue, hideAvailableBalance = false }: AvailableActionsProps) => (
   <div className="flex gap-1 text-sm">
     {max !== undefined && setValue !== undefined && (
       <>
-        <span className="mr-1">Available: {max.toFixed(2)}</span>
+        {hideAvailableBalance ? <></> : <span className="mr-1">Available: {max.toFixed(2)}</span>}
         <button className="text-primary hover:underline" onClick={() => setValue(Number(max) * 0.5)} type="button">
           50%
         </button>
