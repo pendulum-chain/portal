38a184e87de9b9667408f1f5895e7e68967a8504
Kacper Szarkiewicz <szarkiewiczmail@gmail.com>
2024-07-02 20:05:06 +0200

    redirect to wallet install page if it not installed

--- src/components/Wallet/modals/ConnectModal/ConnectModalList/ConnectModalWalletsList/ConnectModalWalletsListItem.tsx
+++ src/components/Wallet/modals/ConnectModal/ConnectModalList/ConnectModalWalletsList/ConnectModalWalletsListItem.tsx
@@ -4,23 +4,20 @@ import { Button } from 'react-daisyui';
 interface WalletButtonProps {
   wallet: Wallet;
   onClick: (wallet: Wallet) => void;
-  makeInstallable?: boolean;
 }
 
 function buttonOnClick(props: WalletButtonProps) {
-  const { wallet, onClick, makeInstallable } = props;
+  const { wallet, onClick } = props;
 
   return wallet.installed
-    ? onClick?.(wallet)
-    : (!wallet.installed && wallet.extensionName === 'talisman') || makeInstallable
-      ? window.open(wallet.installUrl, '_blank', 'noopener,noreferrer')
-      : null;
+    ? onClick?.(wallet) :
+      window.open(wallet.installUrl, '_blank', 'noopener,noreferrer')
 }
 
 export const ConnectModalListWalletsItem = (props: WalletButtonProps) => (
   <Button
     key={props.wallet.extensionName}
-    className="w-full flex justify-center md:justify-start outline-primary"
+    className="flex justify-center w-full md:justify-start outline-primary"
     onClick={() => buttonOnClick(props)}
   >
     <img src={props.wallet.logo.src} alt={props.wallet.logo.alt} width={32} height={32} />
--- src/components/Wallet/modals/ConnectModal/ConnectModalList/ConnectModalWalletsList/index.tsx
+++ src/components/Wallet/modals/ConnectModal/ConnectModalList/ConnectModalWalletsList/index.tsx
@@ -6,22 +6,20 @@ interface ConnectWalletListProps {
   wallets?: Wallet[];
   onClick: (wallet: Wallet) => void;
   onClose: () => void;
-  makeInstallable?: boolean;
 }
 
-export function ConnectModalWalletsList({ wallets, onClick, makeInstallable, onClose }: ConnectWalletListProps) {
+export function ConnectModalWalletsList({ wallets, onClick, onClose }: ConnectWalletListProps) {
   if (!wallets?.length) {
     return <p>No wallet installed</p>;
   }
 
   return (
-    <section className="grid md:grid-cols-2 gap-4">
+    <section className="grid gap-4 md:grid-cols-2">
       {wallets.map((wallet: Wallet) => (
         <ConnectModalListWalletsItem
           key={wallet.extensionName}
           wallet={wallet}
           onClick={onClick}
-          makeInstallable={makeInstallable}
         />
       ))}
       <WalletConnect onClick={onClose} />
