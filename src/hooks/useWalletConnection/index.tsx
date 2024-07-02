8b48b6a34ef747bacfe01e3bc90c0cc9b0807a61
Kacper Szarkiewicz <szarkiewiczmail@gmail.com>
2024-07-02 21:41:31 +0200

    refactor Nova Wallet connection

--- src/hooks/useWalletConnection/index.tsx
+++ src/hooks/useWalletConnection/index.tsx
@@ -1,11 +1,13 @@
 import { useMetamask } from './useMetamask';
 import { useConnectWallet } from './useConnectWallet';
+import { useNova } from './useNova';
 
 export function useWalletConnection() {
   const { wallets = [], accounts = [], selectWallet, loading, selectedWallet } = useConnectWallet();
   const { selectedWallet: metamaskSelectedWallet } = useMetamask();
+  const { selectedWallet: novaSelectedWallet } = useNova()
 
-  const allWallets = [...wallets, metamaskSelectedWallet];
+  const allWallets = [...wallets, metamaskSelectedWallet, novaSelectedWallet];
 
   return { wallets: allWallets, accounts, selectWallet, loading, selectedWallet };
 }
