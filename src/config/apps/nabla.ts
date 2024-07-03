d2cf278996dd843c49c5713d2dde6479855173d4
Torsten Stüber <15174476+TorstenStueber@users.noreply.github.com>
2024-07-03 07:53:14 +0200

    Enable USDC.axl Nabla prototype for Pendulum

--- src/config/apps/nabla.ts
+++ src/config/apps/nabla.ts
@@ -13,11 +13,16 @@ export type NablaConfig = AppConfigBase &
   >;
 
 export const nablaConfig: NablaConfig = {
-  tenants: [TenantName.Foucoco],
-  environment: ['staging', 'development'],
+  tenants: [TenantName.Foucoco, TenantName.Pendulum],
+  environment: ['staging', 'development', 'production'],
   foucoco: {
     indexerUrl: 'https://pendulum.squids.live/foucoco-squid/graphql',
     router: '6h54SUQjbXjYbYT7p7vDcXSVGAQNgu78jkhErLJoW7EXo3ry',
     oracle: '6nT6THovpSsTtCCi9Lae1NoAAviLbzULaEo41Yg5eYJrCZXu',
+  },
+  pendulum: {
+    indexerUrl: 'https://pendulum.squids.live/pendulum-squid/graphql',
+    router: '6dQQoUKQ9LNDCrGMjoZjeHBXsuihSgQiQEgD9Z7VtHR82wfG',
+    oracle: '6eFMEXfr5cebufroDDPnJrawkuoDeqYwxtwn3vKaKLNjM31W',
   },
 };
