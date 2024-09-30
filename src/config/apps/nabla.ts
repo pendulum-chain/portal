3944ce6bf702bb861a85fec7c937589a6f863848
Marcel Ebert <mail@marcel-ebert.de>
2024-09-30 12:20:24 +0200

    Add link to subscan explore on the transaction status pop up (#557)
    * Bump api-solang to latest version
    
    * Show button to view nabla transaction on explorer
    
    * Change margin of buttons
    
    * Enable Nabla on Pendulum (only for testing, to be removed)
    
    * remove unnecessary code
    
    * improve nav responsiveness when multiple collapse groups appear
    
    * fix button class
    
    * change swap footer text
    
    * change collapse behaviour to open only one at the time
    
    * Remove boolean cast with `!!`
    
    * Amend merge
    
    ---------
    
    Co-authored-by: Kacper Szarkiewicz <szarkiewiczmail@gmail.com>
    Co-authored-by: Kacper Szarkiewicz <43585069+Sharqiewicz@users.noreply.github.com>

--- src/config/apps/nabla.ts
+++ src/config/apps/nabla.ts
@@ -1,5 +1,6 @@
 import { TenantName } from '../../models/Tenant';
 import { AppConfigBase } from './types';
+
 export type NablaConfig = AppConfigBase &
   Partial<
     Record<
@@ -13,11 +14,16 @@ export type NablaConfig = AppConfigBase &
   >;
 
 export const nablaConfig: NablaConfig = {
-  tenants: [TenantName.Foucoco],
-  environment: ['staging', 'development'],
+  tenants: [TenantName.Foucoco, TenantName.Pendulum],
+  environment: ['staging', 'development', 'production'],
   foucoco: {
     indexerUrl: 'https://pendulum.squids.live/foucoco-squid/graphql',
     router: '6mYwT4yRrrMK978NszqBvxkvXjYnsmKfs2BkYGJGiR4XY9Sc',
     oracle: '6kqj1tnYUGY3L93YFArXKF2E4tpQ2tUJ4DARwkAjZEyDoof6',
+  },
+  pendulum: {
+    indexerUrl: 'https://pendulum.squids.live/pendulum-squid/graphql',
+    router: '6buMJsFCbXpHRyacKTjBn3Jss241b2aA7CZf9tKzKHMJWpcJ',
+    oracle: '6f9uHwN2r5w82Bjrywc4wmZYb6TN64bZH5Ev87qmJ675uFvq',
   },
 };
