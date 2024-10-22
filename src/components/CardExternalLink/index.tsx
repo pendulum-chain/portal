fe90e112c3651099c6869c904dc00e40a002decd
Kacper Szarkiewicz <szarkiewiczmail@gmail.com>
2024-10-22 16:28:35 +0200

    create useResolvedUrl hook

--- src/components/CardExternalLink/index.tsx
+++ src/components/CardExternalLink/index.tsx
@@ -1,7 +1,7 @@
-import { useState, useEffect } from 'preact/compat';
 import { Card } from 'react-daisyui';
 
 import { ExternalIcon } from '../../assets/ExternalIcon';
+import { useResolvedUrl } from '../../hooks/useResolvedUrl';
 
 export interface CardExternalLinkProps {
   title: string;
@@ -10,18 +10,10 @@ export interface CardExternalLinkProps {
 }
 
 export function CardExternalLink({ children, href }: CardExternalLinkProps) {
-  const [finalHref, setFinalHref] = useState<string>('');
-
-  useEffect(() => {
-    if (typeof href === 'string') {
-      setFinalHref(href);
-    } else {
-      href.then(setFinalHref);
-    }
-  }, [href]);
+  const finalUrl = useResolvedUrl(href);
 
   return (
-    <a href={finalHref} target="_blank" rel="noreferrer">
+    <a href={finalUrl} target="_blank" rel="noreferrer">
       <Card className="mt-2 flex h-20 flex-row items-center rounded-md bg-base-300/60 px-4 hover:opacity-70">
         {children}
         <ExternalIcon className="ml-auto mr-1 h-5 w-5 dark:fill-white" />,
