40f7b39fb935b8352519e5146641208267eef948
Kacper Szarkiewicz <szarkiewiczmail@gmail.com>
2024-10-22 07:25:48 +0200

    change zenlink from component to .svg

--- src/pages/fund-wallet/index.tsx
+++ src/pages/fund-wallet/index.tsx
@@ -4,7 +4,7 @@ import { TenantName } from '../../models/Tenant';
 import { useGlobalState } from '../../GlobalStateProvider';
 import { useEffect, useMemo } from 'preact/compat';
 import { MexcIcon } from '../../assets/mexc';
-import { ZenlinkIcon } from '../../assets/zenlink';
+import zenlinkIcon from '../../assets/exchange/zenlink.svg';
 import stellaswapIcon from '../../assets/exchange/stellaswap.svg';
 import alchemyPayIcon from '../../assets/alchemypay.svg';
 import externalIcon from '../../assets/ExternalIcon.svg';
@@ -18,18 +18,22 @@ const CARD_DATA: Record<TenantName, { buy: CardDetail[]; exchange: CardDetail[] 
     buy: [
       {
         title: 'AlchemyPay',
-        image: <img src={alchemyPayIcon} className="w-full h-full" />,
+        image: <img src={alchemyPayIcon} className="h-full w-full" />,
         href: config.alchemyPay.encodeUrlWithRedirection(config.alchemyPay.prodUrl, window.location.href),
       },
     ],
     exchange: [
-      { title: 'MEXC', image: <MexcIcon className="w-full h-full" />, href: 'https://www.mexc.com/exchange/PEN_USDT' },
+      { title: 'MEXC', image: <MexcIcon className="h-full w-full" />, href: 'https://www.mexc.com/exchange/PEN_USDT' },
       {
         title: 'StellaSwap',
-        image: <img src={stellaswapIcon} className="w-full h-full" />,
+        image: <img src={stellaswapIcon} className="h-full w-full" />,
         href: 'https://app.stellaswap.com/exchange/swap',
       },
-      { title: 'Zenlink', image: <ZenlinkIcon className="w-full h-full" />, href: 'https://app.zenlink.pro/swap' },
+      {
+        title: 'Zenlink',
+        image: <img src={zenlinkIcon} className="h-full w-full" />,
+        href: 'https://app.zenlink.pro/swap',
+      },
     ],
   },
   amplitude: {
@@ -37,7 +41,7 @@ const CARD_DATA: Record<TenantName, { buy: CardDetail[]; exchange: CardDetail[] 
     exchange: [
       {
         title: 'Zenlink',
-        image: <ZenlinkIcon className="w-full h-full" />,
+        image: <img src={zenlinkIcon} className="h-full w-full" />,
         href: 'https://app.zenlink.pro/swap',
       },
     ],
@@ -80,7 +84,7 @@ function ContentCard(props: ContentCardProps) {
 
   return (
     <a href={finalHref} target="_blank" rel="noreferrer">
-      <Card className="flex flex-row items-center px-4 mt-2 rounded-md bg-base-300/60 hover:opacity-70">
+      <Card className="mt-2 flex flex-row items-center rounded-md bg-base-300/60 px-4 hover:opacity-70">
         <div className={`ml-6 h-20 w-40 fill-${fill}`}>{image}</div>
         <img src={externalIcon} className={`ml-auto mr-1 h-5 w-5 fill-${fill}`} />,
       </Card>
@@ -142,13 +146,13 @@ function FundWallet() {
   const tabClassName = 'h-full w-full text-lg sm:text-md text-primary font-bold py-5';
 
   return (
-    <div className="flex items-center justify-center h-full mt-4">
-      <Card bordered className="w-full max-w-xl px-8 py-6 tab-card shadow-0 bg-base-200">
+    <div className="mt-4 flex h-full items-center justify-center">
+      <Card bordered className="tab-card shadow-0 w-full max-w-xl bg-base-200 px-8 py-6">
         <Card.Title tag="h2" className="text-3xl font-normal">
           Fund Wallet
         </Card.Title>
-        <div className="flex justify-between mt-5">
-          <Tabs className="flex justify-center flex-grow p-0 border tabs-boxed border-neutral-500 bg-base-100 sm:w-5/6">
+        <div className="mt-5 flex justify-between">
+          <Tabs className="tabs-boxed flex flex-grow justify-center border border-neutral-500 bg-base-100 p-0 sm:w-5/6">
             <Tabs.Tab className={tabClassName} {...getTabProps(FundWalletTabs.Buy)}>
               Buy
             </Tabs.Tab>
