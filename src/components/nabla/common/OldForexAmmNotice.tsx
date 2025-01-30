ff8d9e998a0603352c53d81e3ce6538e723c7f5d
Marcel Ebert <mail@marcel-ebert.de>
2025-01-30 16:28:05 +0100

    Add component with hint to old forex amm

---
+++ src/components/nabla/common/OldForexAmmNotice.tsx
@@ -0,0 +1,19 @@
+import { InformationCircleIcon } from '@heroicons/react/24/outline';
+
+export function OldForexAmmNotice() {
+  return (
+    <div className="mb-4 flex items-center justify-center gap-2 rounded-md bg-gray-200 p-2 text-gray-800">
+      <InformationCircleIcon className="h-4" />
+      <span>
+        You can find the old instance of the Forex AMM on{' '}
+        <a
+          className="text-primary transition hover:underline"
+          href="https://old-forex-amm--rococo-souffle-a625f5.netlify.app/pendulum/nabla/swap"
+        >
+          this
+        </a>{' '}
+        page.
+      </span>
+    </div>
+  );
+}
