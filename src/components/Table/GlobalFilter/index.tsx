10e033e89ffc69f2f9793772a6a0c82416248b7e
Kacper Szarkiewicz <szarkiewiczmail@gmail.com>
2025-11-28 13:35:59 +0100

    update search styles, search is live update instead of form submit

--- src/components/Table/GlobalFilter/index.tsx
+++ src/components/Table/GlobalFilter/index.tsx
@@ -1,40 +1,27 @@
-import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
-import { FormEvent, useCallback, useRef } from 'react';
-import { Button, Input, Join } from 'react-daisyui';
+import { ChangeEvent } from 'react';
+import { Input } from 'react-daisyui';
 
 export interface GlobalFilterProps {
   globalFilter?: string;
   setGlobalFilter: (val: string) => void;
 }
 export const GlobalFilter = ({ globalFilter, setGlobalFilter }: GlobalFilterProps): JSX.Element => {
-  const inputRef = useRef<HTMLInputElement>(null);
-  const onSearch = useCallback(
-    (event: FormEvent) => {
-      if (event) event.preventDefault();
-      if (!inputRef.current) return;
-      setGlobalFilter(inputRef.current.value);
-    },
-    [setGlobalFilter],
-  );
+  const handleFilterChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setGlobalFilter(event.target.value);
+  };
 
   return (
-    <form onSubmit={onSearch}>
-      <div className="form-control max-w-72">
-        <Join>
-          <Input
-            ref={inputRef}
-            size="sm"
-            bordered
-            type="text"
-            maxLength={128}
-            defaultValue={globalFilter}
-            placeholder="Search..."
-          />
-          <Button size="sm" color="secondary" className="dark:border-neutral-700 dark:bg-neutral-700" type="submit">
-            <MagnifyingGlassIcon className="w-3 h-3" />
-          </Button>
-        </Join>
-      </div>
-    </form>
+    <div className="form-control">
+        <Input
+          className="bg-base-200"
+          size="sm"
+          bordered
+          type="text"
+          maxLength={128}
+          value={globalFilter || ''}
+          onChange={handleFilterChange}
+          placeholder="Search..."
+        />
+    </div>
   );
 };
