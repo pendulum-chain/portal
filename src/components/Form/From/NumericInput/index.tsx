b96f9cf4ccbf8cdb9b985f9bf8b710c1049c61cd
Kacper Szarkiewicz <szarkiewiczmail@gmail.com>
2025-12-21 22:01:34 +0100

    install Numora as the numeric-input package

--- src/components/Form/From/NumericInput/index.tsx
+++ src/components/Form/From/NumericInput/index.tsx
@@ -1,9 +1,8 @@
-import { Input } from 'react-daisyui';
 import { UseFormRegisterReturn } from 'react-hook-form';
+import { NumoraInput } from 'numora-react';
+import { ChangeEvent, ClipboardEvent } from 'react';
 
 import { USER_INPUT_MAX_DECIMALS } from '../../../../shared/parseNumbers/maxDecimals';
-import { handleOnChangeNumericInput, handleOnKeyDownNumericInput, handleOnPasteNumericInput } from './helpers';
-import { ChangeEvent, ClipboardEvent } from 'react';
 
 interface NumericInputProps {
   register: UseFormRegisterReturn;
@@ -22,41 +21,30 @@ export const NumericInput = ({
   defaultValue,
   autoFocus,
 }: NumericInputProps) => {
-  function handleOnChange(e: ChangeEvent): void {
-    handleOnChangeNumericInput(e, maxDecimals);
-    register.onChange(e);
-  }
-
-  function handleOnPaste(e: ClipboardEvent): void {
-    handleOnPasteNumericInput(e, maxDecimals);
-    register.onChange(e);
+  function handleOnChange(e: ChangeEvent<HTMLInputElement> | ClipboardEvent<HTMLInputElement>): void {
+    register.onChange(e as ChangeEvent<HTMLInputElement>);
   }
 
   return (
     <div className="flex justify-between w-full">
       <div className="flex-grow text-4xl text-black font-outfit">
-        <Input
+        <NumoraInput
           {...register}
           autoComplete="off"
           autoCorrect="off"
           autoCapitalize="none"
           className={
             'font-outfit input-ghost w-full pl-0 text-4xl text-accent-content focus:text-accent-content focus:outline-none ' +
-            additionalStyle
+            (additionalStyle || '')
           }
-          minLength={1}
-          onChange={handleOnChange}
-          onKeyDown={handleOnKeyDownNumericInput}
-          onPaste={handleOnPaste}
-          pattern="^[0-9]*[.,]?[0-9]*$"
           placeholder="0.0"
           readOnly={readOnly}
           spellCheck="false"
-          step="any"
-          type="text"
-          inputMode="decimal"
-          value={defaultValue}
+          maxDecimals={maxDecimals}
+          defaultValue={defaultValue}
           autoFocus={autoFocus}
+          onChange={handleOnChange}
+          enableLeadingZeros={true}
         />
       </div>
     </div>
